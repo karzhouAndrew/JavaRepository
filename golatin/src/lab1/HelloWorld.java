54f5d1a0e60698fece0ff66aaf690ed4524d5541
golatin <golatin@rambler.ru>
2015-04-30 19:38:55 +0200

    Попытка 1

---
+++ golatin/src/lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by User on 30.04.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hallo world");
+    }
+}
