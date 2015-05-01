25bbc49738a45488547e3c1f83c5c762af3757b3
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-01 18:54:26 +0300

    Task 4. Rectangle and circle.

---
+++ Boris/src/laba4/CircleCoverRectangle.java
@@ -0,0 +1,18 @@
+package laba4;
+
+//There is a rectangle outlet with 'a' and 'b' side. Is the circle with radius 'r' a piece of cardboard full covering
+// the outlet?
+public class CircleCoverRectangle {
+    public static void main(String[] args) {
+        int radius = 5;
+        int sideA = 6;
+        int sideB = 8;
+        if ((int) Math.pow(sideA, 2) + (int) Math.pow(sideB, 2) > (int) Math.pow(2 * radius, 2)) {
+            System.out.printf("Круг с радиусом %d не закрывает полностью прямоугольник со сторонами %d и %d",
+                    radius, sideA, sideB);
+        } else {
+            System.out.printf("Круг с радиусом %d полностью закрывает прямоугольник со сторонами %d и %d",
+                    radius, sideA, sideB);
+        }
+    }
+}
