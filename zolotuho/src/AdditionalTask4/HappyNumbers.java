91b29babb64ef860f7af0654bc0fb48d9a98efee
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-01 23:09:45 +0300

    Task4 additional

---
+++ zolotuho/src/AdditionalTask4/HappyNumbers.java
@@ -0,0 +1,78 @@
+package AdditionalTask4;
+
+import java.util.Scanner;
+
+public class HappyNumbers {
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner(System.in);
+        System.out.print("Enter three-digit number : ");
+        int number=sc.nextInt();
+        switch (number){
+            case 111:
+                System.out.println("happy");
+                break;
+            case 222:
+                System.out.println("happy");
+                break;
+            case 333:
+                System.out.println("happy");
+                break;
+            case 444:
+                System.out.println("happy");
+                break;
+            case 555:
+                System.out.println("happy");
+                break;
+            case 666:
+                System.out.println("happy");
+                break;
+            case 777:
+                System.out.println("happy");
+                break;
+            case 888:
+                System.out.println("happy");
+                break;
+            case 999:
+                System.out.println("happy");
+                break;
+            case 123:
+                System.out.println("happy");
+                break;
+            case 234:
+                System.out.println("happy");
+                break;
+            case 345:
+                System.out.println("happy");
+                break;
+            case 456:
+                System.out.println("happy");
+                break;
+            case 567:
+                System.out.println("happy");
+                break;
+            case 678:
+                System.out.println("happy");
+                break;
+            case 789:
+                System.out.println("happy");
+                break;
+
+            case 781:
+                System.out.println("magic");
+                break;
+            case 302:
+                System.out.println("magic");
+                break;
+            case 409:
+                System.out.println("magic");
+                break;
+            case 941:
+                System.out.println("magic");
+                break;
+            default:
+                System.out.println("Not happy");
+        }
+
+
+    }
+}
