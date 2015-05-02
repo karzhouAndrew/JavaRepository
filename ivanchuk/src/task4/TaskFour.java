a1375c09d56efeaffa8aa8164778c0464931ff03
Дмитрий <dimkaiv@list.ru>
2015-05-02 15:28:39 +0300

    This fourth task.

--- ivanchuk/src/task4/TaskFour.java
+++ ivanchuk/src/task4/TaskFour.java
@@ -9,15 +9,14 @@ public class TaskFour {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите ширину и высоту прямоугольного отвестия.");
-        int width= in.nextInt();
+        int width = in.nextInt();
         int height = in.nextInt();
         System.out.println("Введите радиус картонки");
         int radius = in.nextInt();
-        double hypotenuse = Math.sqrt(Math.pow(width,2)+Math.pow(height,2));
-        if(2*radius > hypotenuse) {
+        double hypotenuse = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+        if (2 * radius > hypotenuse) {
             System.out.println("Да, отверстие можно закрыть картонкой радиусом " + radius);
-        }
-        else {
+        } else {
             System.out.println("Нет, отверстие нельзя закрыть картонкой радиусом " + radius);
         }
     }
