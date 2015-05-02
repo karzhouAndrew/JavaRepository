c2e1c1522b640e2d2cf5656aa3e4b8690094abf9
Константин <k336699k@mail.ru>
2015-05-02 20:14:45 +0300

    Лабораторная работа 3

--- Shaplyko/src/lab3/HelloWorld.java
+++ Shaplyko/src/lab3/HelloWorld.java
@@ -3,12 +3,12 @@ package lab3;
 /**
  * Задание 3
  * Создайте число. Определите, является ли последняя цифра числа семеркой.
+ * можно попробовать такой алагритм
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        int number = 7;
-        int num = number + 3;
-        if (num % 2 == 0 && num % 5 == 0) {
+        int number = 15;
+        if (number % 10 == 7) {
             System.out.println("Число" + number + "является числом с последней цифрой семь");
         } else {
             System.out.println("Число" + number + "не оканчивается на семь");
