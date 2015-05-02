aa9ceefa1006baa861c378c7f5b3ac58f894e31f
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-02 21:31:38 +0300

    Task 18. Changed. Formatting. First number of sequence not "0".

--- Boris/src/laba8/MultiplySequence.java
+++ Boris/src/laba8/MultiplySequence.java
@@ -3,16 +3,13 @@ package laba8;
 //Find factorial natural number.
 public class MultiplySequence {
     public static void main(String[] args) {
-        int randomValue = (int) (Math.random() * 10);
+        int randomValue = (int) (Math.random() * 9) + 1;
         int multiply = 1;
-        if (randomValue==0){
-            multiply=0;
-        }
         while (randomValue != 0) {
-            System.out.print(" "+randomValue);
+            System.out.print(" " + randomValue);
             multiply *= randomValue;
             randomValue = (int) (Math.random() * 10);
         }
-        System.out.println("\n Произведение чисел последовательности равна "+multiply);
+        System.out.println("\n Произведение чисел последовательности равна " + multiply);
     }
 }
