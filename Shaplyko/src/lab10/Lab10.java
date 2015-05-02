ec153a7d37366cb46e48a68d12eee327beba7d84
Константин <k336699k@mail.ru>
2015-05-02 21:13:23 +0300

    Лабораторная работа 10

--- Shaplyko/src/lab10/Lab10.java
+++ Shaplyko/src/lab10/Lab10.java
@@ -2,14 +2,13 @@ package lab10;
 
 /**
  * Задание 10
- Имеется целое число, определить является ли это число простым, т.е. делится без остатка только на 1 и себя.
-
+ * Имеется целое число, определить является ли это число простым, т.е. делится без остатка только на 1 и себя.
  */
 public class Lab10 {
     public static void main(String[] args) {
-        int number = 3;
+        int number = 111;
         int n = 0;
-        for (int i = 1; i <= number; i++) {
+        for (int i = 1; i <= (int) Math.sqrt(number); i++) {
             if (number % i == 0) {
                 n += 1;
             }
