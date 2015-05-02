582e04802cdeb7bca5f54c0efae9d4a55fd81b13
golatin <golatin@rambler.ru>
2015-05-02 09:55:02 +0200

    Домашнее задание 6

---
+++ golatin/src/workGroup/HomeWorkNumber6.java
@@ -0,0 +1,14 @@
+package workGroup;
+
+/**
+ * Created by User on 02.05.2015.
+ * Определить число, полученное выписыванием в обратном порядке цифр любого натурального числа n.
+ */
+public class HomeWorkNumber6 {
+    public static void main(String[] args) {
+        int number = 34356967;
+        int antiNumber = 0;
+        for (; number > 0; antiNumber = 10 * antiNumber + number % 10, number /= 10) ;
+        System.out.println(antiNumber);
+    }
+}
