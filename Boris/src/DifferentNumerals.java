26ff84148d202bfad0c3f84756894dcde787a284
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-02 18:30:55 +0300

    Task 7. Different numerals of number.

---
+++ Boris/src/DifferentNumerals.java
@@ -0,0 +1,28 @@
+
+//There is natural number. Is it all literals different?
+public class DifferentNumerals {
+    public static void main(String[] args) {
+        int number = 1234584;
+        int numberChange = number;
+        boolean flag = false;
+        while (numberChange > 10) {
+            int numeralLast = numberChange % 10;
+            int numberRemainder = numberChange / 10;
+            while (numberRemainder != 0) {
+                if (numeralLast == (numberRemainder % 10)) {
+                    flag = true;
+                    break;
+                }
+                numberRemainder /= 10;
+            }
+            numberChange /= 10;
+            if (flag) {
+                System.out.println("Число " + number + " содержит одинаковые цифры");
+                break;
+            } else if (numberChange < 10) {
+                System.out.println("Число " + number + " не содержит одинаковых цифр");
+                break;
+            }
+        }
+    }
+}
