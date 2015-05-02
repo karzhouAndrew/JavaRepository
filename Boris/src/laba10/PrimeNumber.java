010f91f8abde25f99da23286db574d787044ecb4
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-02 21:41:18 +0300

    Task 10. Changed. Formatting. На 1 нам делить не надо, т.к. все числа и так делятся на 1. Соответственно следующее число 2. Следующее условие позволяет не проверять деление на числа кратные 2, естественно кроме самой 2-ки.

--- Boris/src/laba10/PrimeNumber.java
+++ Boris/src/laba10/PrimeNumber.java
@@ -1,6 +1,7 @@
 package laba10;
 
 //There is natural number. Is it "prime number"?
+
 import java.util.Random;
 
 public class PrimeNumber {
@@ -8,7 +9,7 @@ public class PrimeNumber {
         Random rand = new Random();
         int number = rand.nextInt(999) + 2;
         boolean flag = true;
-        for (int i = 2; i < Math.sqrt(number)+1; i++) {
+        for (int i = 2; i < Math.sqrt(number) + 1; i++) {
             if (i != 2 && i % 2 == 0) {
                 continue;
             }
