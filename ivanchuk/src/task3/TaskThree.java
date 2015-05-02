9fc8c18b25f6155c92eecaa392fcedc960d67f63
Дмитрий <dimkaiv@list.ru>
2015-05-02 19:52:17 +0300

    This corrected third task.

--- ivanchuk/src/task3/TaskThree.java
+++ ivanchuk/src/task3/TaskThree.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 //Определите, является ли последняя цифра числа семеркой.
 public class TaskThree {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         System.out.println("Введите целое число.");
+        Scanner in = new Scanner(System.in);
+        in.close();
         int number = in.nextInt();
         if (number % 10 == 7) {
             System.out.println("Да, последняя цифра числа семерка.");
