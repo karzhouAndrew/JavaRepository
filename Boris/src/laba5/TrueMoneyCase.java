ee14459da3685e09067fbec4a1f2e9a89cb3521f
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-02 16:42:10 +0300

    Task 5. Define right case for word.

---
+++ Boris/src/laba5/TrueMoneyCase.java
@@ -0,0 +1,21 @@
+package laba5;
+
+//There is integer number. It's quantity money in roubles. Take out this number and add right case word "rouble".
+public class TrueMoneyCase {
+    public static void main(String[] args) {
+        int quantityMoney = 101;
+        int lastNumber = quantityMoney % 10;
+        int lastNextNumber = (quantityMoney % 100 - lastNumber) / 10;
+        String rublsCase = "рубл";
+        if (lastNextNumber == 1) {
+            rublsCase += "ей";
+        } else if (lastNumber == 1) {
+            rublsCase += "ь";
+        } else if (lastNumber == 2 || lastNumber == 3 || lastNumber == 4) {
+            rublsCase += "я";
+        } else {
+            rublsCase += "ей";
+        }
+        System.out.println(quantityMoney + " " + rublsCase);
+    }
+}
