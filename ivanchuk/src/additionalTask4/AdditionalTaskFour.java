42f7b76c9fa11d6ec75175083b167d8722c30cb2
Дмитрий <dimkaiv@list.ru>
2015-05-02 18:28:02 +0300

    This fourth additional task.

---
+++ ivanchuk/src/additionalTask4/AdditionalTaskFour.java
@@ -0,0 +1,42 @@
+package additionalTask4;
+
+// На вход программе поступает трёхзначное число (от 100 до 999),
+// которое генерируется случайным образом либо вводиться из консоли (на свое усмотрение).
+// Необходимо определить является ли число счастливым.
+// Счастливое число, это число у которого все цифры совпадают (например 777),
+// либо число у которого каждая последующая цифра на 1 больше предыдущей (123 или 456),
+// а также счастливыми являются числа 781, 302, 409 и 941 (такие числа будет называть “Магическими”).
+// Программа должна вывести на экран входное число ( только в случае если оно было сгенерировано) и
+// результат какое число попалось: “Счастливое”, “Несчастливое” или “Магическое”.
+
+public class AdditionalTaskFour {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 899 + 100);
+        switch (number) {
+            case 781:
+                System.out.println("Число " + number + " - \"Магическое\".");
+                break;
+            case 302:
+                System.out.println("Число " + number + " - \"Магическое\".");
+                break;
+            case 409:
+                System.out.println("Число " + number + " - \"Магическое\".");
+                break;
+            case 941:
+                System.out.println("Число " + number + " - \"Магическое\".");
+                break;
+            default:
+                int[] array = new int[3];
+                for (int i = 0; i < array.length; i++) {
+                    array[i] = (int) (number % Math.pow(10, i + 1) / Math.pow(10, i));
+                }
+                boolean coincidence = array[0] == array[1] && array[0] == array[2];
+                boolean increase = array[2] == array[1] - 1 && array[1] == array[0] - 1;
+                if (coincidence || increase) {
+                    System.out.println("Число " + number + " - \"Счастливое\".");
+                } else {
+                    System.out.println("Число " + number + " - \"Несчастливое\".");
+                }
+        }
+    }
+}
