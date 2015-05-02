ac7629212d22465cf988e35754f30051e79eca98
golatin <golatin@rambler.ru>
2015-05-02 09:59:15 +0200

    Домашнее задание 7

---
+++ golatin/src/workGroup/HomeWorkNumber7.java
@@ -0,0 +1,19 @@
+package workGroup;
+
+/**
+ * Created by User on 02.05.2015.
+ * Дано  любое натуральное  n. Верно ли, что все  цифры числа различны?
+ */
+public class HomeWorkNumber7 {
+    public static void main(String[] args) {
+        int number = 1245785;
+        String result = "Совпадений нет";
+        for (int flags = 0; number > 0; flags |= 1 << (number % 10), number /= 10) {
+            if ((flags & (1 << (number % 10))) != 0) {
+                result = "Совпадение в цифре: " + number % 10;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
