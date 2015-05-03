3e1ac6543ed2307add337cdd2888ae41bad1c734
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-03 22:22:07 +0300

    Lab14

--- gladchenkov/src/Lab14/Lab14.java
+++ gladchenkov/src/Lab14/Lab14.java
@@ -6,34 +6,33 @@ package Lab14;
 public class Lab14 {
     public static void main(String[] args) {
         int marks[] = {2, 1, 2, 2, 15, 7, 8, 3, 21, 20};
-        int maxMark = marks[0];
-        int minMark = Integer.MAX_VALUE;
-        int minIndex = 0;
-        int maxIndex = 0;
-        int sum = 0;
-
 
         for (int i = 0; i < marks.length; i++) {
             System.out.println("Ученик № " + i + " = " + marks[i]);
         }
-        for (int i = 0; i < marks.length; i++) {  //поиск максимального числа в массиве
+
+        int maxMark = marks[0];
+        int maxIndex = 0;
+        for (int i = 0; i < marks.length; i++) {
             if (maxMark < marks[i]) {
                 maxMark = marks[i];
                 maxIndex = i;
             }
         }
 
-        for (int i = 0; i < marks.length; i++) {  //поиск минимального числа в массиве
+        int minMark = Integer.MAX_VALUE;
+        int minIndex = 0;
+        for (int i = 0; i < marks.length; i++) {
             if (minMark > marks[i]) {
                 minMark = marks[i];
                 minIndex = i;
             }
         }
-
-        for (int i = minIndex + 1; i < maxIndex; i++) { //сумма между минимальным значением и максимальным
+        int sum = 0;
+        for (int i = minIndex + 1; i < maxIndex; i++) {
             sum = sum + marks[i];
         }
-        System.out.println("Сумма чисел между минимальным индексом " + minIndex + " и максимальным " + maxIndex + " = " + +sum);
+        System.out.println("Сумма между " + minIndex + " и " + maxIndex + " = " + sum);
 
     }
 }
