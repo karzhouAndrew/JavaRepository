37f5af298b1b3cae28f5315c900d55c9855bb684
dudinsky <dudinsky@tut.by>
2015-05-03 21:32:55 +0300

    Task06_Version2

--- Dudinsky/src/lab06/Task06.java
+++ Dudinsky/src/lab06/Task06.java
@@ -3,9 +3,9 @@ package lab06;
 /* Определить число, полученное выписыванием в обратном порядке цифр любого натурального числа n. */
 public class Task06 {
     public static void main(String[] args) {
-        long givenNumber = 1908417453;
+        int givenNumber = 190847453;
         System.out.println("Заданное число = " + givenNumber);
-        long resultNumber = givenNumber % 10;
+        int resultNumber = givenNumber % 10;
         while (givenNumber > 9) {
             givenNumber /= 10;
             resultNumber = resultNumber * 10 + givenNumber % 10;
