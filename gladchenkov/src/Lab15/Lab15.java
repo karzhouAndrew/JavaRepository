5478572d8238ea8b3c733d092d452d8aaadf4d16
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-03 22:23:43 +0300

    Lab15

--- gladchenkov/src/Lab15/Lab15.java
+++ gladchenkov/src/Lab15/Lab15.java
@@ -7,15 +7,15 @@ public class Lab15 {
     public static void main(String[] args) {
         int[][] array = new int[5][5];
 
-        for (int i = 0; i < array.length / 2 + 1; i++) {   //заполнение
+        for (int i = 0; i < array.length / 2 + 1; i++) {
             for (int j = i; j < array.length - i; j++) {
                 array[i][j] = 1;
                 array[array.length - i - 1][j] = 1;
             }
         }
-        for (int[] anArray : array) {   //вывод массива
+        for (int[] arrayElement : array) {
             for (int j = 0; j < array.length; j++) {
-                System.out.print(anArray[j]);
+                System.out.print(arrayElement[j]);
             }
             System.out.println();
         }
