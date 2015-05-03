371c7205ae1bc3dc6249ba393bec7fd634933979
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-03 12:10:33 +0300

    Task 15. Changed.

--- Boris/src/laba15/ButterflyMatrix.java
+++ Boris/src/laba15/ButterflyMatrix.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 //Create square matrix and fill it "butterfly".
 public class ButterflyMatrix {
     public static void main(String[] args) {
-        int value = 7;
-        int[][] arrayButterfly = new int[value][value];
+        int arraySize = 7;
+        int[][] arrayButterfly = new int[arraySize][arraySize];
         for (int i = 0; i < arrayButterfly.length / 2.0; i++) {
             for (int j = i; j < arrayButterfly[i].length - i; j++) {
                 arrayButterfly[i][j] = 1;
