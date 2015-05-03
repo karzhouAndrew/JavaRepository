40705a12b4b0226a857af58619cdeb34ec9e111a
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-03 23:56:39 +0300

    Lab3

--- gladchenkov/src/lab3/Lab3.java
+++ gladchenkov/src/lab3/Lab3.java
@@ -5,8 +5,8 @@ package lab3;
  */
 public class Lab3 {
     public static void main(String[] args) {
-        int a = 204678;
-        if (a % 10 == 7) {
+        int number = 204678;
+        if (number % 10 == 7) {
             System.out.println("Псоледняя цифра числа равна 7");
         } else {
             System.out.println("Последняя цифра числа не равна 7");
