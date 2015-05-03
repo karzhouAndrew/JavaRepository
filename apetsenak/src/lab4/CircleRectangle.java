837f0f1f78c125f5199b82b2595b134ffb3459cd
Vitali Apetsenak <vitali.apetsenak@gmail.com>
2015-05-03 14:10:55 +0300

    Task 4. Circle and rectangle.

---
+++ apetsenak/src/lab4/CircleRectangle.java
@@ -0,0 +1,18 @@
+package lab4;
+
+/**
+ * Имеется прямоугольное отверстие размерами a и b, определить,
+ * можно ли его полностью закрыть круглой картонкой радиусом r.
+ */
+public class CircleRectangle {
+    public static void main(String[] args) {
+        int height = 3;
+        int width = 6;
+        int radius = 5;
+        if (radius >= Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2))) {
+            System.out.println("Картонка радиуса r закроет отверстие.");
+        } else {
+            System.out.println("Картонка радиуса r не закроет отверстие.");
+        }
+    }
+}
