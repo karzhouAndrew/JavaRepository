053b8142865e8e38444f0330d198f3c56f6b2af4
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-03 23:33:51 +0300

    Lab16

--- gladchenkov/src/Lab16/Lab16.java
+++ gladchenkov/src/Lab16/Lab16.java
@@ -1,9 +1,9 @@
 package Lab16;
 
 public class Lab16 {
-    private int counter = 0;
 
     public void search(String str) {
+        int counter = 0;
         for (int i = 0; i < str.length(); i++) {
             char symbol = str.charAt(i);
             if (symbol == ',' || symbol == ':' || symbol == '!') {
