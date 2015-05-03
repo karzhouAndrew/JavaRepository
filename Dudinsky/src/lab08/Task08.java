15ce64eea0b15defdb53294db017bf968b650edc
dudinsky <dudinsky@tut.by>
2015-05-03 22:47:56 +0300

    Task08_Version2

--- Dudinsky/src/lab08/Task08.java
+++ Dudinsky/src/lab08/Task08.java
@@ -1,14 +1,17 @@
 package lab08;
 
+import java.util.Random;
+
 /* Задание 8. Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а произведение, т.е. факториал числа. */
 public class Task08 {
     public static void main(String[] args) {
         double factorial = 1;
-        int x = (int) (Math.random() * 20);
-        System.out.print("Факториал числа " + x);
-        while (x != 0) {
-            factorial *= x;
-            x--;
+        Random rand = new Random();
+        int inputNumber = rand.nextInt(100);
+        System.out.print("Факториал числа " + inputNumber);
+        while (inputNumber != 0) {
+            factorial *= inputNumber;
+            inputNumber--;
         }
         System.out.println(" имеет значение = " + factorial);
     }
