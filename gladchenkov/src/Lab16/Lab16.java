eba484281ddc8bb167af979864618e3fe5a7e5a9
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-03 20:41:47 +0300

    Lab16. Search and calculation of punctuation marks

--- gladchenkov/src/Lab16/Lab16.java
+++ gladchenkov/src/Lab16/Lab16.java
@@ -3,15 +3,15 @@ package Lab16;
 public class Lab16 {
 
     private String str = "  Тестовая, строка,! с несколькими,!, запятыми:   ";
-    private int n = 0;
+    private int counter = 0;
     private char symbol;
 
     public void search() {
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
             if (symbol == ',' || symbol == ':' || symbol == '!')
-                n++;
+                counter++;
         }
-        System.out.println("У нас есть " + n + " знаков ");
+        System.out.println("У нас есть " + counter + " знаков ");
     }
 }
