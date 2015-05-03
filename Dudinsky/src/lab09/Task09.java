4398c89ada2e9f94c244d535836c0542390a680e
dudinsky <dudinsky@tut.by>
2015-05-03 23:03:22 +0300

    Task09_Version2

--- Dudinsky/src/lab09/Task09.java
+++ Dudinsky/src/lab09/Task09.java
@@ -3,18 +3,18 @@ package lab09;
 /* Как в примере, имеется последовательность случайных чисел, найти и вывести наибольшее из них. */
 public class Task09 {
     public static void main(String[] args) {
-        int x = (int) (Math.random() * 20);
-        int maxX = x;
+        int inputNumber = (int) (Math.random() * 20);
+        int maxNumber = inputNumber;
         System.out.println("Последовательность случайных чисел от 0 до 19 (генерируется пока не попадется нуль):");
-        System.out.print(x + " ");
-        while (x != 0) {
-            x = (int) (Math.random() * 20);
-            System.out.print(x + " ");
-            if (x > maxX) {
-                maxX = x;
+        System.out.print(inputNumber + " ");
+        while (inputNumber != 0) {
+            inputNumber = (int) (Math.random() * 20);
+            System.out.print(inputNumber + " ");
+            if (inputNumber > maxNumber) {
+                maxNumber = inputNumber;
             }
         }
         System.out.println();
-        System.out.println("Наибольшее число в последовательности: " + maxX);
+        System.out.println("Наибольшее число в последовательности: " + maxNumber);
     }
 }
