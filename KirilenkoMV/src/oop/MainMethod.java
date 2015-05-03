4247e5c08a61a325c7c9d01e705cbfb6e44e6c88
KirilenkoMV <Kirilenko_MV@bytechs.by>
2015-05-03 19:32:01 +0300

    Класс Куб, посчитать объем, площадь, изменить цвет.

---
+++ KirilenkoMV/src/oop/MainMethod.java
@@ -0,0 +1,15 @@
+package oop;
+
+/**
+ * Created by asus on 03.05.2015.
+ */
+public class MainMethod {
+    public static void main(String[] args) {
+        Cube cube = new Cube(20.0,10.0,30.0);
+        double s = cube.CalulateS();
+        System.out.println("Площадь "+s);
+        double v = cube.CalulateV();
+        System.out.println("Объем "+v);
+        cube.setColor("Red");
+    }
+}
