0d3e912d94c089732e1dc7ff0e97198be5d6a49c
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-04 00:30:07 +0300

    Lab19

--- gladchenkov/src/Lab19/Lab19Main.java
+++ gladchenkov/src/Lab19/Lab19Main.java
@@ -1,5 +1,9 @@
 package Lab19;
 
+/**
+ * Напишите два цикла выполняющих многократное сложение строк, один с помощью оператора с
+ * ложения и String, а другой с помощью StringBuilder и метода append. Сравните скорость их выполнения.
+ */
 public class Lab19Main {
     public static void main(String[] args) {
         Lab19 lab = new Lab19();
