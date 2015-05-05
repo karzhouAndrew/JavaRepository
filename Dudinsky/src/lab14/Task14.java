a832f9697158af25fc27578f6be6a190b3e42727
dudinsky_a <dudinsky@tut.by>
2015-05-05 16:35:35 +0300

    Task14

---
+++ Dudinsky/src/lab14/Task14.java
@@ -0,0 +1,50 @@
+package lab14;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/* Задание 14. Определите сумму элементов одномерного массива, расположенных между минимальным и максимальным значениями. */
+/* КОНКРЕТИЗИРОВАЛ ЗАДАНИЕ: при наличии элементов с равными значениями в качестве границ суммируемого диапазона программа примет
+   первый из равных минимальных и последний из равных максимальных; в сумму не включаю сами граничные элементы */
+public class Task14 {
+    public static void main(String[] args) {
+        int[] arrMinToMax = new int[12];
+        int arrayLength = arrMinToMax.length;
+        Random rand = new Random();
+        for (int i = 0; i < arrayLength; i++) {
+            arrMinToMax[i] = rand.nextInt(10);
+        }
+        System.out.println("Исходный массив случайных чисел:\n" + Arrays.toString(arrMinToMax));
+        int minIndex = 0;
+        int minValue = arrMinToMax[0];
+        for (int i = 0; i < arrayLength; i++) {
+            if (arrMinToMax[i] < minValue) {
+                minValue = arrMinToMax[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("Первый из элементов с минимальным значением " + minValue + " имеет индекс " + minIndex);
+        int maxIndex = arrayLength - 1;
+        int maxValue = arrMinToMax[arrayLength - 1];
+        for (int i = arrayLength - 1; i >= 0; i--) {
+            if (arrMinToMax[i] > maxValue) {
+                maxValue = arrMinToMax[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Последний из элементов с максимальным значением " + maxValue + " имеет индекс " + maxIndex);
+        int startOfRange, endOfRange;
+        if (minIndex < maxIndex) {
+            startOfRange = minIndex;
+            endOfRange = maxIndex;
+        } else {
+            startOfRange = maxIndex;
+            endOfRange = minIndex;
+        }
+        int sum = 0;
+        for (int i = startOfRange + 1; i < endOfRange; i++) {
+            sum += arrMinToMax[i];
+        }
+        System.out.println("Сумма элементов массива между ними (не включая сами граничные элементы) равна: " + sum);
+    }
+}
