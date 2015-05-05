95b6022b1339132213d3c2c3159cac977d4e62e0
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-05 23:30:26 +0300

    Test

---
+++ Boris/src/labaTest/Test1.java
@@ -0,0 +1,11 @@
+package labaTest;
+
+/**
+ * Created by Администратор on 05.05.2015.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Proverka");
+    }
+}
