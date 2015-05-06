ee6901f933ca21d4a0677527e41766c893a4b1e6
Vitali Apetsenak <vitali.apetsenak@gmail.com>
2015-05-06 20:29:58 +0300

    Task5.

--- apetsenak/src/lab5/Task.java
+++ apetsenak/src/lab5/Task.java
@@ -7,7 +7,7 @@ package lab5;
 public class Task {
     public static void main(String[] args) {
         int number = 111;
-        if (number % 10 == 0|| number % 10 > 4 || (number % 100 > 10 && number % 100 < 15)) {
+        if (number % 10 == 0 || number % 10 > 4 || (number % 100 > 10 && number % 100 < 15)) {
             System.out.println(number + " рублей");
         } else if (number % 10 == 1) {
             System.out.println(number + " рубль");
