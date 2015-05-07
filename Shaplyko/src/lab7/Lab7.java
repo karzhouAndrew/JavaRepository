a4e2b7ea10d8460f7fdfb43ed83414090ec2e1bc
Константин <k336699k@mail.ru>
2015-05-07 11:21:09 +0300

    Лабораторная работа 7

--- Shaplyko/src/lab7/Lab7.java
+++ Shaplyko/src/lab7/Lab7.java
@@ -2,75 +2,40 @@ package lab7;
 
 /**
  * Задание 7
- Дано  любое натуральное  n. Верно ли, что все  цифры числа раз-личны?
-Для шестизначного числа
+ * Дано  любое натуральное  n. Верно ли, что все  цифры числа раз-личны?
+ * Для шестизначного числа
  */
 public class Lab7 {
     public static void main(String[] args) {
-        int number = 133456;
-        int num12;
-        int num13;
-        int num14;
-        int num15;
-        int num16;
-        int num17;
-        int num1 = number % 10;
-        int num11 = (number - number % 10) / 10;
-        if (num11 > 0) {
-            num11 = num11 % 10;
-            num12 = (number - number % 100) / 100;
-            if (num12 > 0) {
-                num12 = num12 % 10;
-                num13 = (number - number % 1000) / 1000;
-                if (num13 > 0) {
-                    num13 = num13 % 10;
-                    num14 = (number - number % 10000) / 10000;
-                    if (num14 > 0) {
-                        num14 = num14 % 10;
-                        num15 = (number - number % 100000) / 100000;
-                        if (num15 > 0) {
-                            num15 = num15 % 10;
-                            num16 = (number - number % 1000000) / 1000000;
-                            if (num16 > 0) {
-                                num16 = num16 % 10;
-                                num17 = (number - number % 10000000) / 10000000;
-                            } else {
-                                if (num11 == num1 || num11 == num12 || num11 == num13 || num11 == num14 || num11 == num15 || num12 == num1 || num12 == num13 || num12 == num14 || num12 == num15 || num13 == num14 || num13 == num15 || num13 == num1 || num1 == num14 || num14 == num15 || num1 == num15){
-                                    System.out.println( "Да, совпадение");
-                                } else {
-                                    System.out.println( "Нет, они различны");
-                                }
-                            }
-                        } else {
-                            if (num11 == num1 || num11 == num12 || num11 == num13 || num11 == num14 || num12 == num1 || num12 == num13 || num12 == num14 || num13 == num14 || num1 == num14 || num1 == num13 ) {
-                                System.out.println( "Да, совпадение");
-                            } else {
-                                System.out.println( "Нет, они различны");
-                            }
-                        }
-                    } else {
-                        if (num11 == num1 || num11 == num12 || num11 == num13 || num12 == num1 || num12 == num13 || num1 == num13) {
-                            System.out.println( "Да, совпадение");
-                        } else {
-                            System.out.println( "Нет, они различны");
-                        }
-                    }
-                } else {
-                    if (num11 == num1 || num11 == num12 || num12 == num1 ) {
-                        System.out.println( "Да, совпадение");
-                    } else {
-                        System.out.println( "Нет, они различны");
-                    }
-                }
-            } else {
-                if (num11 == num1) {
-                    System.out.println( "Да, совпадение");
-                } else {
-                    System.out.println( "Нет, они различны");
+        int number = 12306;
+        int i = 0;   //  количество повторений цикла 1
+        int n = 0;    //  количество совпадений (равенство) цифр
+        int numberLoop1 = number;
+        do {
+            int numberDigitLoop1 = numberLoop1 % 10;
+            int numberShortLoop1 = (numberLoop1 - numberLoop1 % 10) / 10;
+            numberLoop1 = numberShortLoop1;
+            int numberLoop2 = number;
+            while (numberLoop2 > 0) {
+                int numberDigitLoop2 = numberLoop2 % 10;
+                int numberShortLoop2 = (numberLoop2 - numberLoop2 % 10) / 10;
+                numberLoop2 = numberShortLoop2;
+                if (numberDigitLoop1 == numberDigitLoop2) {
+                    n++;
                 }
             }
+            i++;
+        } while (numberLoop1 > 0);
+        if (i == 1) {
+            System.out.println("У вас число, состоящее из одной цифры");
         } else {
-            System.out.println( "У нас только одно число " +  num1);
+            if (n > i) {
+                System.out.println("Да, совпадение");
+            } else {
+                System.out.println("Нет, они различны");
+            }
         }
     }
 }
+
+
