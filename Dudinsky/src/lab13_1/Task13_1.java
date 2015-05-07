6048faf04c4eb9d13df49765e5d703c36956b891
dudinsky_a <dudinsky@tut.by>
2015-05-07 09:52:13 +0300

    Task13_1_version2

--- Dudinsky/src/lab13_1/Task13_1.java
+++ Dudinsky/src/lab13_1/Task13_1.java
@@ -1,36 +1,33 @@
 package lab13_1;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /* Задание 13-1. Создать квадратный двумерный массив, заполнить его случайными элементами, распечатать, повернуть на 90 градусов по часовой, и снова распечатать (при переворачивании нежелательно создавать еще один массив). */
 public class Task13_1 {
     public static void main(String[] args) {
-        int arrayToTurnLength = 5;
-        int[][] arrayToTurn = new int[arrayToTurnLength][arrayToTurnLength];
+        final int SIZE = 5;
+        int[][] arrayToTurn = new int[SIZE][SIZE];
         Random rand = new Random();
         System.out.println("Исходный квадратный двумерный массив случайных чисел:");
-        for (int i = 0; i < arrayToTurnLength; i++) {
-            for (int j = 0; j < arrayToTurnLength; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 arrayToTurn[i][j] = rand.nextInt(10);
-                System.out.print(arrayToTurn[i][j] + " ");
             }
-            System.out.println();
+            System.out.println(Arrays.toString(arrayToTurn[i]));
         }
-        for (int i = 0; i < arrayToTurnLength / 2; i++) {
-            for (int j = i; j < arrayToTurnLength - 1 - i; j++) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            for (int j = i; j < SIZE - 1 - i; j++) {
                 int tempVar = arrayToTurn[i][j];
-                arrayToTurn[i][j] = arrayToTurn[arrayToTurnLength - 1 - j][i];
-                arrayToTurn[arrayToTurnLength - 1 - j][i] = arrayToTurn[arrayToTurnLength - 1 - i][arrayToTurnLength - 1 - j];
-                arrayToTurn[arrayToTurnLength - 1 - i][arrayToTurnLength - 1 - j] = arrayToTurn[j][arrayToTurnLength - 1 - i];
-                arrayToTurn[j][arrayToTurnLength - 1 - i] = tempVar;
+                arrayToTurn[i][j] = arrayToTurn[SIZE - 1 - j][i];
+                arrayToTurn[SIZE - 1 - j][i] = arrayToTurn[SIZE - 1 - i][SIZE - 1 - j];
+                arrayToTurn[SIZE - 1 - i][SIZE - 1 - j] = arrayToTurn[j][SIZE - 1 - i];
+                arrayToTurn[j][SIZE - 1 - i] = tempVar;
             }
         }
         System.out.println("Результат его поворота на 90 градусов по часовой стрелке: ");
-        for (int i = 0; i < arrayToTurnLength; i++) {
-            for (int j = 0; j < arrayToTurnLength; j++) {
-                System.out.print(arrayToTurn[i][j] + " ");
-            }
-            System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(Arrays.toString(arrayToTurn[i]));
         }
     }
 }
