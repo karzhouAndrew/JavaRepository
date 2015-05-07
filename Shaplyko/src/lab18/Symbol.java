cc8018d698e30cab130f24f073f20d473cbef17d
Константин <k336699k@mail.ru>
2015-05-07 11:50:26 +0300

    Лабораторная работа 18

--- Shaplyko/src/lab18/Symbol.java
+++ Shaplyko/src/lab18/Symbol.java
@@ -4,10 +4,10 @@ package lab18;
  * Created by Константин on 06.05.2015.
  */
 public class Symbol {
-    public void word(String a) {
-        for (int i = 1; i < a.length(); i++) {
-            if (a.charAt(i) == ' ' && a.charAt(i - 1) != ' ') {
-                System.out.print(a.charAt(i - 1));
+    public void word(String strEnter) {
+        for (int i = 1; i < strEnter.length(); i++) {
+            if (strEnter.charAt(i) == ' ' && strEnter.charAt(i - 1) != ' ') {
+                System.out.print(strEnter.charAt(i - 1));
             }
         }
     }
