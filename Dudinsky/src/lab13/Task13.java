2255dc36ff4788c21522c7d4f0d084b848d1843a
dudinsky_a <dudinsky@tut.by>
2015-05-07 09:26:08 +0300

    Task13_version2

--- Dudinsky/src/lab13/Task13.java
+++ Dudinsky/src/lab13/Task13.java
@@ -6,17 +6,17 @@ import java.util.Random;
 /* Задание 13. Создать массив, заполнить его случайными элементами, распечатать, перевернуть, и снова распечатать (при переворачивании нежелательно создавать еще один массив). */
 public class Task13 {
     public static void main(String[] args) {
-        int[] arrayToReverse = new int[12];
-        int arrayToRevLength = arrayToReverse.length;
+        final int SIZE = 12;
+        int[] arrayToReverse = new int[SIZE];
         Random rand = new Random();
-        for (int i = 0; i < arrayToRevLength; i++) {
+        for (int i = 0; i < SIZE; i++) {
             arrayToReverse[i] = rand.nextInt(100);
         }
         System.out.println("Исходный массив случайных чисел:\n" + Arrays.toString(arrayToReverse));
-        for (int i = 0; i < arrayToRevLength / 2; i++) {
+        for (int i = 0; i < SIZE / 2; i++) {
             int tempVar = arrayToReverse[i];
-            arrayToReverse[i] = arrayToReverse[arrayToRevLength - 1 - i];
-            arrayToReverse[arrayToRevLength - 1 - i] = tempVar;
+            arrayToReverse[i] = arrayToReverse[SIZE - 1 - i];
+            arrayToReverse[SIZE - 1 - i] = tempVar;
         }
         System.out.println("Результат переворота массива наоборот:\n" + Arrays.toString(arrayToReverse));
     }
