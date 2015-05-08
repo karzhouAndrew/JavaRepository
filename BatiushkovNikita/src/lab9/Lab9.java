0ea83733a05f5852fb7da27590bce58eccb9f0a0
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-08 13:24:43 +0300

    Lab9 Maximum value of digits in the sequence

--- BatiushkovNikita/src/lab9/Lab9.java
+++ BatiushkovNikita/src/lab9/Lab9.java
@@ -6,13 +6,13 @@ public class Lab9 {
     public static void main(String[] args) {
         int randNum = (int) (Math.random() * 10);
         int maxValue = 0;
-        do {
+        while (randNum != 0) {
             randNum = (int) (Math.random() * 10);
             System.out.println("Random x = " + randNum);
             if (maxValue <= randNum) {
                 maxValue = randNum;
             }
-        } while (randNum != 0);
+        }
         System.out.println("Maximum value of x: " + maxValue);
     }
 }
