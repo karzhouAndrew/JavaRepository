afffe44407cbc59b6daa1b49fb218e607ac1dacb
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-08 10:03:07 +0300

    Lab6 Made without strings

--- BatiushkovNikita/src/lab6/Lab6.java
+++ BatiushkovNikita/src/lab6/Lab6.java
@@ -4,13 +4,13 @@ package lab6;
 
 public class Lab6 {
     public static void main(String[] args) {
-        int number = 987111;
-        String strNumModified = "";
-        while (number / 10 != 0 || number % 10 != 0) {
-            strNumModified += Integer.toString(number % 10);
+        int number = 1234;
+        int reverseNum = 0;
+        while (number != 0) {
+            reverseNum = reverseNum * 10 + number % 10;
+            number -= number % 10;
             number /= 10;
         }
-        int NumModified = Integer.parseInt(strNumModified);
-        System.out.println(NumModified);
+        System.out.printf("Reverse number = " + reverseNum);
     }
 }
