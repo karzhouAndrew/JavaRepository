c66bc61cbd2d50fb198b546b27df6dcd77474219
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-08 08:12:42 +0300

    Lab4 Brackets have been corrected

--- BatiushkovNikita/src/lab4/Lab4.java
+++ BatiushkovNikita/src/lab4/Lab4.java
@@ -7,7 +7,7 @@ public class Lab4 {
         double a = 2;
         double b = 2;
         double r = 1.5;
-        if ((r * 2) * (r * 2) <= (a * a) + (b * b)) {
+        if (r * 2 * r * 2 <= a * a + b * b) {
             System.out.println("True");
         } else {
             System.out.println("False");
