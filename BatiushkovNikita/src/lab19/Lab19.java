7db9449e7a50ce6c1607152fccf865e21419a708
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-09 13:43:00 +0300

    Lab 19 Performance (+ String) and (StringBuilder + append)

--- BatiushkovNikita/src/lab19/Lab19.java
+++ BatiushkovNikita/src/lab19/Lab19.java
@@ -1,7 +1,30 @@
 package lab19;
 
-/**
- * Created by Xirus on 09.05.2015.
- */
+// Performance (+ String) and (StringBuilder + append)
+
 public class Lab19 {
+    public static void main(String[] args) {
+        int numOfCycles = 100;
+        String strAdd = "add ";
+
+        String strStringTest = "";
+        long startStringTest = System.nanoTime();
+        for (int i = 0; i < numOfCycles; i++) {
+            strStringTest += strAdd;
+        }
+        long stopStringTest = System.nanoTime();
+        long perfString = stopStringTest - startStringTest;
+        System.out.println("Performance + and String  = " + perfString);
+
+        StringBuilder strBuilderTest = new StringBuilder();
+        long startBuilderTest = System.nanoTime();
+        for (int i = 0; i < numOfCycles; i++) {
+            strBuilderTest.append(strAdd);
+        }
+        long stopBuilderTest = System.nanoTime();
+        long perfStrBulder = stopBuilderTest - startBuilderTest;
+        System.out.println("Performance append() and StringBulser = " + perfStrBulder);
+
+        System.out.println(perfString / perfStrBulder + " times faster.");
+    }
 }
