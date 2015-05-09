47e80cd1ea67100cf1148a6941c812c614c0870e
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-09 19:02:18 +0300

    Lab 17 Refactor with method

--- BatiushkovNikita/src/lab17/Lab17.java
+++ BatiushkovNikita/src/lab17/Lab17.java
@@ -5,10 +5,10 @@ package lab17;
 public class Lab17 {
     public static void main(String[] args) {
         String str = "  The String:;       class,, represents. character!? strings. zzuz ";
-        System.out.println(WordCounter(str));
+        System.out.println(wordCounter(str));
     }
 
-    private static int WordCounter(String str) {
+    private static int wordCounter(String str) {
         str = str.trim();
         str = str.replaceAll("\\s+", " ");
         int strLength = str.length();
