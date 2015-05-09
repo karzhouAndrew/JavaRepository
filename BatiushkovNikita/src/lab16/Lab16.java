2ea41e7e534edd3abb95653bf870eae33eb32175
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-09 18:49:56 +0300

    Lab 16

--- BatiushkovNikita/src/lab16/Lab16.java
+++ BatiushkovNikita/src/lab16/Lab16.java
@@ -7,9 +7,8 @@ public class Lab16 {
         String str = "The String:; class,, represents. character!? strings. ";
         int count = 0;
         int strLength = str.length();
-        char symbol;
         for (int i = 0; i < strLength; i++) {
-            symbol = str.charAt(i);
+            char symbol = str.charAt(i);
             if (symbol == (',') || symbol == '!' || symbol == '?' ||
                     symbol == ';' || symbol == ':' || symbol == '.') {
                 count++;
