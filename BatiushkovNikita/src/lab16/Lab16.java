0f9e0962f6b2099a0b56360a4979b9aacb3d7654
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-09 10:10:00 +0300

    Lab 16 Find other punctuation marks in string

--- BatiushkovNikita/src/lab16/Lab16.java
+++ BatiushkovNikita/src/lab16/Lab16.java
@@ -1,12 +1,21 @@
 package lab16;
 
-/**
- * Created by Xirus on 08.05.2015.
- */
+// Find other punctuation marks in string
 
 public class Lab16 {
     public static void main(String[] args) {
-
+        String str = "The String:; class,, represents. character!? strings. ";
+        int count = 0;
+        int strLength = str.length();
+        char symbol;
+        for (int i = 0; i < strLength; i++) {
+            symbol = str.charAt(i);
+            if (symbol == (',') || symbol == '!' || symbol == '?' ||
+                    symbol == ';' || symbol == ':' || symbol == '.') {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
 
