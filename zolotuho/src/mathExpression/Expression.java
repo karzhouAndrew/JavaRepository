9edcb72dee0a5c9cee26b4efc4d87e9b3ff8e7bf
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-09 13:06:38 +0300

    Calculate  mathematical expressions

---
+++ zolotuho/src/mathExpression/Expression.java
@@ -0,0 +1,16 @@
+package mathExpression;
+
+import static java.lang.Math.*;
+
+public class Expression {
+
+    public static double longExpression(double x) {
+        double expr1 = pow(cos(pow(E, x) + pow(E, pow(x, 2)) + 1 / sqrt(x)), -4);
+        double expr2 = pow(cos(PI * pow(x, 3)) + log1p(pow(x, 2)), sin(x));
+        return expr1 / expr2;
+    }
+
+    public static double shortExpression(double x, double t, double s) {
+        return (sin(sin(pow(x, t)))) / sqrt(1 + pow(x, s));
+    }
+}
