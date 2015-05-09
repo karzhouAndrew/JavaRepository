94b41c5d799d475546643f214b4f4731758d7a38
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-09 10:57:51 +0300

    Lab 17 The number of words in the text

---
+++ BatiushkovNikita/src/lab18/Lab18.java
@@ -0,0 +1,9 @@
+package lab18;
+
+
+
+public class Lab18 {
+    public static void main(String[] args) {
+        String str = "  The String:;       class,, represents. character!? strings. zzuz ";
+    }
+}
