b051dced4e168bf428bff83ea35884a414652d3f
Константин <k336699k@mail.ru>
2015-05-10 17:54:17 +0300

    Лабораторная работа 15

--- Shaplyko/src/lab15/Lab15.java
+++ Shaplyko/src/lab15/Lab15.java
@@ -15,7 +15,6 @@ public class Lab15 {
         for (int i = 0; i < array.length / 2; i++) {
             for (int j = 0; j < array.length; j++) {
                 if ((j < i) || (j >= (array[i].length - i))) {
-                    // array[i][j] = 0;
                 } else {
                     array[i][j] = 1;
                 }
@@ -24,7 +23,6 @@ public class Lab15 {
         for (int i = array.length - 1; i >= array.length / 2; i--) {
             for (int j = 0; j < array.length; j++) {
                 if ((j < (array.length - 1 - i)) || (j > i)) {
-                    // array[i][j] = 0;
                 } else {
                     array[i][j] = 1;
                 }
