352501a84a5ce3da27b84701dfd25bea0e25e880
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-10 20:26:17 +0300

    ExtraLab07

--- BatiushkovNikita/src/extraLab07/ExtraLab07.java
+++ BatiushkovNikita/src/extraLab07/ExtraLab07.java
@@ -3,25 +3,30 @@ package extraLab07;
 // 1. The number of consonants and vowels.
 //
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExtraLab07 {
     public static void main(String[] args) {
-        String text = "В один прекрасный день заказчику потребовалось выяснить, какие классы использует его приложение. " +
-                "Приложение было уже хорошо знакомо автору и представляло собой гремучую смесь из кода различной расовой принадлежности, реализующего (к чести разработчиков системы, по большей части грамотно и к месту) механизмы наследования, позднего связывания и динамической компиляции. " +
-                "Поэтому информация о действительно используемых классах могла существенно помочь в рефакторинге приложения.\n" +
-                "Задача поставлена следующим образом: в процессе работы приложения должен формироваться файл, содержащий имена всех классов, непосредственно использованных приложением." +
-                "\rОно, к слову, состоит из двух основных частей: сервера приложений, на котором размещен веб-интерфейс приложения, и сервера обработки (отдельный сервер, на котором различные периодические задачи запускаются с помощью скриптов Ant). " +
-                "Разумеется, информацию о классах необходимо собирать с обеих частей приложения.\n" +
-                "Приступим к поиску решения поставленной задачи и заодно разберемся с механизмами загрузки классов в Java.";
-        String text2 = "В один прекрасный день заказчику потребовалось выяснить, какие классы испол";
-        System.out.println("Количество гласных = " + getNumVowels(text2));
-        System.out.println("Количество согласных = " + getNumConsonants(text2));
+        String text = "Жили-были мужик да баба. Оба были такие ленивые... Так и норовят дело на чужие плечи столкнуть, самим бы только не делать... И дверь-то в избу никогда на крюк не закладывали: утром-де вставай да руки протягивай, да опять крюк скидывай... И так проживем.\n" +
+                "Вот раз баба и свари каши. А уж и каша сварилась! Румяна рассыпчата, крупина от крупины так и отваливается.";
+
+        System.out.println("The number of vowels = " + getNumVowels(text));
+        System.out.println("The number of consonants = " + getNumConsonants(text));
+        getSentenseSequence(getSentenceArr(text));
 
     }
-    private static String getSentence(String str) {
-        return "";
+
+    private static void getSentenseSequence(String[] arrStr) {
+        int arrStrLength = arrStr.length;
+        for (int i = 0; i < arrStrLength; i++) {
+            System.out.println(arrStr[i] + ". [Sentence №" + (i + 1) + ". Length: " + arrStr[i].length() + "]");
+        }
+    }
+
+    private static String[] getSentenceArr(String str) {
+        return str.split("[.]+");
     }
 
     private static int getNumConsonants(String str) {
