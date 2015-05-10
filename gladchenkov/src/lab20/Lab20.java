cdf9269bfaa56a744c3585c4f3ba262e81837526
Sergey Gladchenkov <sergg85@gmail.com>
2015-05-10 13:19:22 +0300

    Lab20

--- gladchenkov/src/lab20/Lab20.java
+++ gladchenkov/src/lab20/Lab20.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lab20 {
-    public void SearchNumbers(String str) {
+    public void searchNumbers(String str) {
         Pattern p = Pattern.compile("0x[0-9a-fA-F]+");
         Matcher m = p.matcher(str);
         while (m.find()) {
--- gladchenkov/src/lab20/Lab20Main.java
+++ gladchenkov/src/lab20/Lab20Main.java
@@ -7,6 +7,6 @@ package lab20;
 public class Lab20Main {
     public static void main(String[] args) {
         Lab20 lab = new Lab20();
-        lab.SearchNumbers("0x66 Просто 0x78текст 0xAB  содержащий 0x15");
+        lab.searchNumbers("0x66 Просто 0x78текст 0xAB  содержащий 0x15");
     }
 }
