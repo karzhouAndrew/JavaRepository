19091b516608de7615b92c5aa6441855a805c614
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-10 14:33:03 +0300

    ExtraLab06

--- BatiushkovNikita/src/extraLab06/ExtraLab06.java
+++ BatiushkovNikita/src/extraLab06/ExtraLab06.java
@@ -1,6 +1,16 @@
 package extraLab06;
 
-//
+// Find the value of the function
 
 public class ExtraLab06 {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(firstExpression(x));
+    }
+
+    private static double firstExpression(int x) {
+        double y;
+        y = Math.cos(Math.exp(x))*Math.sin(x)/Math.pow(x, 2)*Math.E;
+        return y;
+    }
 }
