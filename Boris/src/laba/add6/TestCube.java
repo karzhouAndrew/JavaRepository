9635cf88f74ca43155fb2dbef219ba2f5acac2b2
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-11 22:27:19 +0300

    Task add6.

--- Boris/src/laba/add6/TestCube.java
+++ Boris/src/laba/add6/TestCube.java
@@ -12,9 +12,9 @@ public class TestCube {
         modelCube1.setSide(-3);
         modelCube1.setSide(3.5);
         modelCube1.setColor("Red");
-        System.out.println(
+        System.out.println("" +
                 "\nПлощадь куба равна " + modelCube1.getArea() + ".\nОбъем куба равен " + modelCube1.getCapacity() +
-                        ".\nЦвет куба равен " + modelCube1.getColor() + ".\nСторона равна " + modelCube1.getSide() + ".");
+                ".\nЦвет куба равен " + modelCube1.getColor() + ".\nСторона равна " + modelCube1.getSide() + ".");
         Cube modelCube2 = new Cube(0);
         System.out.println(
                 "\nПлощадь куба равна " + modelCube2.getArea() + ".\nОбъем куба равен " + modelCube2.getCapacity() +
