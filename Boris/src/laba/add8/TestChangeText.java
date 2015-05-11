f5568f8860f1c18a5fc641cb31cce7f976189227
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-11 22:42:44 +0300

    Task add8.

---
+++ Boris/src/laba/add8/TestChangeText.java
@@ -0,0 +1,10 @@
+package laba.add8;
+
+//Проверка класса ChangeText
+public class TestChangeText {
+    public static void main(String[] args) {
+        String strExample = "Child234 sit in the23 coner. Oth234er old children go ho234me.";
+        System.out.println(strExample);
+        System.out.println(ChangeText.change(strExample));
+    }
+}
