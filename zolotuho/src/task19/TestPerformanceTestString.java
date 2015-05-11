4c5b985a732e86aced734b7c0520488b08873437
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-11 18:02:00 +0300

    19th task

---
+++ zolotuho/src/task19/TestPerformanceTestString.java
@@ -0,0 +1,16 @@
+package task19;
+
+
+public class TestPerformanceTestString {
+    public static void main(String[] args) {
+        int loopLimit = 1000;
+        PerformanceTestString test = new PerformanceTestString();
+        long timeString = test.calculateExecutionTime("qwe", "ert", loopLimit);
+        long timeStringBuilder = test.calculateExecutionTime("qwe", "ert", loopLimit);
+        System.out.println("Execution time of string concatenating =     " + timeString);
+        System.out.println("Execution time of stringBuilder appending =  " + timeStringBuilder);
+        System.out.println("Divided = "+((float)timeString/timeStringBuilder));
+    }
+
+
+}
