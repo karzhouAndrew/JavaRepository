6b02043d25c133bb17d75ddf5138dab2d227d716
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-11 22:19:41 +0300

    Task add6_1.

---
+++ Boris/src/laba/add6_1/TestFormulas.java
@@ -0,0 +1,14 @@
+package laba.add6_1;
+
+//Проверка формул
+public class TestFormulas {
+    public static void main(String[] args) {
+        int argX = 3;
+        TwoFormulas firstFormulaValue = new TwoFormulas(argX);
+        System.out.println("Первое значение формулы при x = " + argX + " равно " + firstFormulaValue.getResultFirst());
+        int paramS = 1;
+        int paramT = 1;
+        System.out.println("Второе значение формулы при x = " + argX + ",s = " + paramS + " и t = " + paramT
+                + " равно " + firstFormulaValue.getResultSecond(paramS, paramT));
+    }
+}
