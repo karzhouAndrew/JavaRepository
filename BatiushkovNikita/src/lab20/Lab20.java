fdbd81bf331ad15cf7bdfbf12c02b911f33ce08b
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-11 23:03:47 +0300

    Lab20 Find hex rev.3

---
+++ BatiushkovNikita/src/lab20/Lab20.java
@@ -0,0 +1,25 @@
+package lab20;
+
+// Find hex
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Lab20 {
+    public static void main(String[] args) {
+        String testString = "Aetu0x9A5F the inde0x12 this 0xF5 0xA7string0x59850xA0853AB0xGC56gur";
+        String regEx = "(0x[0-9A-Fa-f]{2})";
+        System.out.println(getRegExpModifiedString(testString, regEx));
+    }
+
+    private static StringBuilder getRegExpModifiedString(String testString, String regEx) {
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(testString);
+        StringBuilder modifiedString = new StringBuilder();
+        while (matcher.find()) {
+            modifiedString.append(matcher.group());
+            modifiedString.append(" ");
+        }
+        return modifiedString;
+    }
+}
