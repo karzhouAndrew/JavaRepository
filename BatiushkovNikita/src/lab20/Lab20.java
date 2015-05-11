0e1dc528877a1a156ebf483215ec14bfd5b7edf3
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-11 11:29:07 +0300

    Lab20 rev.2

--- BatiushkovNikita/src/lab20/Lab20.java
+++ BatiushkovNikita/src/lab20/Lab20.java
@@ -9,19 +9,17 @@ public class Lab20 {
     public static void main(String[] args) {
         String testString = "Aetu0x9A5F the inde0x12 this 0xF5 0xA7string0x59850xA0853AB0xGC56gur";
         String regEx = "(0x[0-9A-Fa-f]{2})";
-        String modifiedString = getRegExpModifiedString(testString, regEx);
+        StringBuilder modifiedString = getRegExpModifiedString(testString, regEx);
         System.out.println(modifiedString);
     }
 
-    private static String getRegExpModifiedString(String testString, String regEx) {
+    private static StringBuilder getRegExpModifiedString(String testString, String regEx) {
         Pattern pattern = Pattern.compile(regEx);
         Matcher matcher = pattern.matcher(testString);
         StringBuilder modifiedString = new StringBuilder();
-        //String modifiedString = "";
         while (matcher.find()) {
             modifiedString.append(matcher.group());
-            //modifiedString += matcher.group();
-            //modifiedString += " ";
+            modifiedString.append(" ");
         }
         return modifiedString;
     }
