d1c41992ac9e5e5c58c7cfcd7c1c58bf187f2e13
VitaliApetsenak <vitali.apetsenak@gmail.com>
2015-05-12 15:53:06 +0300

    Task18

--- apetsenak/src/lab18/Task.java
+++ apetsenak/src/lab18/Task.java
@@ -8,19 +8,9 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        WorkWithText text = new WorkWithText();
         System.out.println("Введите текст : ");
-        String text = sc.nextLine();
-        String word = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (i != 0 && (text.charAt(i) == ' ' || text.charAt(i) == '.' || text.charAt(i) == ',')) {
-                if (text.charAt(i - 1) != ' ' && text.charAt(i - 1) != '.' && text.charAt(i - 1) != ',') {
-                    word = word.concat(text.substring(i - 1, i));
-                }
-            } else if (i + 1 == text.length()) {
-                word = word.concat(text.substring(i, i + 1));
-            }
-        }
-        System.out.println("word = " + word);
+        System.out.println("Текст из последних букв слов - " + text.createNewText(sc.nextLine()));
         sc.close();
     }
 }
