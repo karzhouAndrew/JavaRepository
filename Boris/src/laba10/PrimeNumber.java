e797bccdf9356c6cde2a973bb06a8eb81c346ed5
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-12 06:48:03 +0300

    Task 10. Changed. Delete test "2". Rename "flag" - noMultiple. Loop last way - when i==Math.sqrt().

--- Boris/src/laba10/PrimeNumber.java
+++ Boris/src/laba10/PrimeNumber.java
@@ -7,18 +7,18 @@ import java.util.Random;
 public class PrimeNumber {
     public static void main(String[] args) {
         Random rand = new Random();
-        int number = rand.nextInt(999) + 2;
-        boolean flag = true;
-        for (int i = 2; i < Math.sqrt(number) + 1; i++) {
+        int number = 49;//rand.nextInt(999) + 2;
+        boolean noMultiple = true;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (i != 2 && i % 2 == 0) {
                 continue;
             }
             if (number % i == 0) {
-                flag = false;
+                noMultiple = false;
                 break;
             }
         }
-        if (flag) {
+        if (noMultiple) {
             System.out.println("Число " + number + " простое.");
         } else {
             System.out.println("Число " + number + " не простое.");
