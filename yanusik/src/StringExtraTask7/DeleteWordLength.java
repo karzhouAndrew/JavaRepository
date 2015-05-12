4f8c54188928d6c27d2a600f2f55027a3284ccb4
dmitry yanusik <yanusikdima@mail.ru>
2015-05-12 20:09:24 +0300

    StringExtraTask7

---
+++ yanusik/src/StringExtraTask7/DeleteWordLength.java
@@ -0,0 +1,14 @@
+package StringExtraTask7;
+
+/**
+ * Created by dmitry on 12.5.15.
+ */
+public class DeleteWordLength {
+    public static void main(String[] args) {
+    String str = "Название файла играет очень важную роль в Java." +
+            " Компилятор Java требует, чтобы мы использовали расширение Java.";
+    String newStr= str.replaceAll("\\b[a-zA-Zа-яА-Я]{4,4}\\b", "");
+        System.out.println(newStr);
+}
+}
+
