8786cc9bb52ce3be9effb717dc020717fbea670a
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-12 23:37:18 +0300

    Classwork task2.

---
+++ Boris/src/work/laba2/SortSentenceRise.java
@@ -0,0 +1,68 @@
+package work.laba2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//2.	Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
+public class SortSentenceRise {
+    private static final Pattern PATTERNSENTENCE = Pattern.compile("[A-ZА-Я][^.?!]+[.?!]+");
+
+    public static String getSentence(String str) {
+        StringBuilder sortString = new StringBuilder("");
+        String[] arraySentence = str.split("[A-ZА-Я][^.?!]+[.?!]+");//Надо подумать, почему не работает один сплит
+        getArraySentence(str, arraySentence);
+        int[] arrayQuantityWord = new int[arraySentence.length];
+        loopForArrayInitialize(arraySentence, arrayQuantityWord);
+        for (int i = 0; i < arrayQuantityWord.length; i++) {
+            putMaxValueLeft(arraySentence, arrayQuantityWord, i);
+            sortString.append(arraySentence[i]);
+            sortString.append("\n");
+        }
+        return sortString.toString();
+    }
+
+    private static void putMaxValueLeft(String[] arraySentence, int[] arrayQuantityWord, int i) {
+        int maxValue = arrayQuantityWord[i];
+        int indexMax = i;
+        for (int j = i; j < arrayQuantityWord.length; j++) {
+            if (maxValue < arrayQuantityWord[j]) {
+                indexMax = j;
+                maxValue = arrayQuantityWord[indexMax];
+            }
+        }
+        if (i != indexMax) {
+            swapArray(arrayQuantityWord, arraySentence, i, indexMax);
+        }
+    }
+
+    private static void swapArray(int[] arrayInt, String[] arrayString, int first, int second) {
+        int tempInt = arrayInt[first];
+        arrayInt[first] = arrayInt[second];
+        arrayInt[second] = tempInt;
+        String tempString = arrayString[first];
+        arrayString[first] = arrayString[second];
+        arrayString[second] = tempString;
+    }
+
+    private static void getArraySentence(String str, String[] arraySentence) {
+        Matcher matcher = PATTERNSENTENCE.matcher(str);
+        int index = 0;
+        while (matcher.find()) {
+            arraySentence[index] = matcher.group();
+            index++;
+        }
+    }
+
+    private static void loopForArrayInitialize(String[] arraySentence, int[] arrayQuantityWord) {
+        for (int i = 0; i < arraySentence.length; i++) {
+            arrayQuantityWord[i] = SortSentenceRise.getQuantityWord(arraySentence[i]);
+        }
+    }
+
+    private static int getQuantityWord(String str) {
+        String[] arrayWords = str.split("[ \\p{Punct}]+");
+        return arrayWords.length;
+    }
+
+}
