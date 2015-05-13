85128cadc41c521564b8af24acea024e34bc2549
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-13 19:41:42 +0300

    Task 10. 3-d changed.

--- Boris/src/laba10/PrimeNumber.java
+++ Boris/src/laba10/PrimeNumber.java
@@ -7,21 +7,22 @@ import java.util.Random;
 public class PrimeNumber {
     public static void main(String[] args) {
         Random rand = new Random();
-        int number = 49;//rand.nextInt(999) + 2;
-        boolean noMultiple = true;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (i != 2 && i % 2 == 0) {
-                continue;
+        int number = rand.nextInt(999);
+        boolean isPrime = true;
+        if (number > 1) {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            if (number % i == 0) {
-                noMultiple = false;
-                break;
+            if (isPrime) {
+                System.out.println("Число " + number + " простое.");
+            } else {
+                System.out.println("Число " + number + " не простое.");
             }
-        }
-        if (noMultiple) {
-            System.out.println("Число " + number + " простое.");
         } else {
-            System.out.println("Число " + number + " не простое.");
+            System.out.println("Введенное число должно быть больше 2.");
         }
     }
 }
