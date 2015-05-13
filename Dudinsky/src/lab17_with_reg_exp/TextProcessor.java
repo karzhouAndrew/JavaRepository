9d16004e73ab8db935f46c5ec152473fc050d9cb
dudinsky_a <dudinsky@tut.by>
2015-05-13 10:44:48 +0300

    Task17_with_regular_expression_version_2

--- Dudinsky/src/lab17_with_reg_exp/TextProcessor.java
+++ Dudinsky/src/lab17_with_reg_exp/TextProcessor.java
@@ -4,9 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextProcessor {
+    private final String WORD_COUNT_PATTERN = "[А-Яа-яЁёA-Za-z]+";
+
     public int calculateWords(String givenStr) {
         int wordCounter = 0;
-        Pattern patternWord = Pattern.compile("[А-Яа-яЁёA-Za-z]+");
+        Pattern patternWord = Pattern.compile(WORD_COUNT_PATTERN);
         Matcher matcher = patternWord.matcher(givenStr);
         while (matcher.find()) {
             wordCounter++;
