681cd45dd70ff4b48fe2acfecb9c8055d1cebdf8
Константин <k336699k@mail.ru>
2015-05-13 09:39:19 +0300

    Лабораторная работа 5

--- Shaplyko/src/lab5/Lab5.java
+++ Shaplyko/src/lab5/Lab5.java
@@ -2,8 +2,8 @@ package lab5;
 
 /**
  * Задание 5
- * Имеется целое число (любое), это число — сумма денег в рублях. Вывести это число, добавив к нему слово «рублей» в правильном падеже.
- * работает до 6 значного числа - но смысл понятен
+ * Имеется целое число (любое), это число — сумма денег в рублях. Вывести это число, добавив к нему слово
+ * «рублей» в правильном падеже.
  */
 public class Lab5 {
     public static void main(String[] args) {
@@ -11,10 +11,16 @@ public class Lab5 {
         int nextNum = (number - number % 10) / 10;
         if (nextNum % 10 == 1) {
             System.out.println(number + " рублей");
-        } else if (number % 10 == 1) {
-            System.out.println(number + " рубль");
-        } else if (number % 10 >= 2 && number % 10 <= 4) {
-            System.out.println(number + " рубля");
-        } else System.out.println(number + " рублей");
+        } else {
+            if (number % 10 == 1) {
+                System.out.println(number + " рубль");
+            } else {
+                if (number % 10 >= 2 && number % 10 <= 4) {
+                    System.out.println(number + " рубля");
+                } else {
+                    System.out.println(number + " рублей");
+                }
+            }
+        }
     }
 }
