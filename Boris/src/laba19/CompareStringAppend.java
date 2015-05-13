b5a791af3202cff8681c4f8e04009861ee46782e
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-13 20:33:55 +0300

    Task 19. 2-d changed. Add Buffer, quantity loop iteration. Change value name.

--- Boris/src/laba19/CompareStringAppend.java
+++ Boris/src/laba19/CompareStringAppend.java
@@ -2,38 +2,50 @@ package laba19;
 
 //Сравнение двух методов на производительность.
 public class CompareStringAppend {
-    private String str;
 
-    CompareStringAppend(String str) {
-        this.str = str;
+
+    public static void timeLoopString(String str, int quantityIteration) {
+        if (quantityIteration < 1) {
+            quantityIteration = 1;
+        }
+        System.out.println("Время выполнения цикла обычным сложением равно "
+                + timeLoop(1, str, quantityIteration) + " секунд.");
+        System.out.println("Время выполнения цикла через Builder и append равно "
+                + timeLoop(2, str, quantityIteration) + " секунд.");
+        System.out.println("Время выполнения цикла через Buffer и append равно "
+                + timeLoop(3, str, quantityIteration) + " секунд.");
     }
 
-    public void timeLoopString() {
-        System.out.println("Время выполнения цикла обычным сложением равно " + timeLoop(true) + " секунд.");
-        System.out.println("Время выполнения цикла через Builder и append равно " + timeLoop(false) + " секунд.");
-    }
-
-    private double timeLoop(boolean method) {
+    private static double timeLoop(int method, String str, int quantityIteration) {
         long timeStart = System.nanoTime();
-        if (method) {
-            loopString();
+        if (method == 1) {
+            loopString(str, quantityIteration);
+        } else if (method == 2) {
+            loopStringBuilder(str, quantityIteration);
         } else {
-            loopStringBuider();
+            loopStringBuffer(str, quantityIteration);
         }
         return (System.nanoTime() - timeStart) / 1E9;
     }
 
-    private void loopString() {
-        String string1 = str;
-        for (int i = 0; i < 1000; i++) {
-            string1 += str;
+    private static void loopStringBuffer(String str, int quantityIteration) {
+        StringBuffer stringBuffer = new StringBuffer(str);
+        for (int i = 0; i < quantityIteration; i++) {
+            stringBuffer.append(str);
         }
     }
 
-    private void loopStringBuider() {
-        StringBuilder string1 = new StringBuilder(str);
-        for (int i = 0; i < 1000; i++) {
-            string1.append(str);
+    private static void loopString(String str, int quantityIteration) {
+        String stringOrdinary = str;
+        for (int i = 0; i < quantityIteration; i++) {
+            stringOrdinary += str;
+        }
+    }
+
+    private static void loopStringBuilder(String str, int quantityIteration) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        for (int i = 0; i < quantityIteration; i++) {
+            stringBuilder.append(str);
         }
     }
 }
--- Boris/src/laba19/TestCompareString.java
+++ Boris/src/laba19/TestCompareString.java
@@ -5,7 +5,7 @@ package laba19;
 public class TestCompareString {
     public static void main(String[] args) {
         String str = "It's difficult to invent this sentence. ";
-        CompareStringAppend objectTest = new CompareStringAppend(str);
-        objectTest.timeLoopString();
+        int quantityIteration = 1000;
+        CompareStringAppend.timeLoopString(str, quantityIteration);
     }
 }
