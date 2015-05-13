7a0a2a942b4c436433f5674d86e33a93d6d371e0
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-13 20:10:56 +0300

    Task 17. 2-d changed. Add third method.

--- Boris/src/laba17/FindWord.java
+++ Boris/src/laba17/FindWord.java
@@ -1,11 +1,14 @@
 package laba17;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //Подсчитать кол-во слов в тексте.
 public class FindWord {
-    private final static Pattern patternW = Pattern.compile("\\w+");
+    private final static Pattern PATTERNWORD = Pattern.compile("\\w+");
 
     public static int firstRealization(String str) {
         String strExample = str.trim();
@@ -25,11 +28,16 @@ public class FindWord {
     }
 
     public static int secondRealization(String str) {
-        Matcher matcher = patternW.matcher(str);
+        Matcher matcher = PATTERNWORD.matcher(str);
         int quantityWord = 0;
         while (matcher.find()) {
             quantityWord++;
         }
         return quantityWord;
+    }
+
+    public static int thirdRealization(String str) {
+        String[] stringArray = str.trim().split("[^\\w]+");
+        return stringArray.length;
     }
 }
--- Boris/src/laba17/TestFindWord.java
+++ Boris/src/laba17/TestFindWord.java
@@ -3,9 +3,10 @@ package laba17;
 // Проверка класса FindWord. Первый метод не отрабатывает пустую строку.
 public class TestFindWord {
     public static void main(String[] args) {
-        String strTest = " This   is a  special sentences. Thank  you!  ";
+        String strTest = " This   is a  special sentences. Thank  you 123 !  ";
         System.out.println("Представленая строка для обрабоки на ангийском: " + strTest);
         System.out.println("Первый метод. Кол-во слов равно " + FindWord.firstRealization(strTest));
         System.out.println("Второй метод. Кол-во слов равно " + FindWord.secondRealization(strTest));
+        System.out.println("Третий метод. Кол-во слов равно " + FindWord.thirdRealization(strTest));
     }
 }
