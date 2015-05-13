29ba1f923925632848e56bcda2975e82331450ec
VitaliApetsenak <vitali.apetsenak@gmail.com>
2015-05-13 08:58:12 +0300

    Task15v2

--- apetsenak/src/lab15/Task.java
+++ apetsenak/src/lab15/Task.java
@@ -1,5 +1,6 @@
 package lab15;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,24 +11,16 @@ public class Task {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите размер матрицы : ");
         int size = sc.nextInt();
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            if (i < (size - i)) {
-                for (int j = i; j < (size - i); j++) {
-                    matrix[i][j] = 1;
-                }
-            } else {
-                for (int j = size - i - 1; j < i + 1; j++) {
-                    matrix[i][j] = 1;
-                }
-            }
-        }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
         sc.close();
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size / 2 + 1; i++) {
+            for (int j = i; j < size - i; j++) {
+                matrix[i][j] = 1;
+                matrix[size - i - 1][j] = 1;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
     }
 }
