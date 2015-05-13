c21d0e1c63ea63e8d91627b2d7ea88a271df9b14
dudinsky_a <dudinsky@tut.by>
2015-05-13 09:55:29 +0300

    Task20_version2

--- Dudinsky/src/lab20/NumberCatcher.java
+++ Dudinsky/src/lab20/NumberCatcher.java
@@ -4,9 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberCatcher {
+    private final String HEX_CATCH_PATTERN = "-?0[xX]\\p{XDigit}+";
 
     public String catchAllHexes(String givenStr) {
-        Pattern patternHex = Pattern.compile("-?0[xX]\\p{XDigit}+");
+        Pattern patternHex = Pattern.compile(HEX_CATCH_PATTERN);
         Matcher matcher = patternHex.matcher(givenStr);
         StringBuilder strBuilder = new StringBuilder("");
         while (matcher.find()) {
