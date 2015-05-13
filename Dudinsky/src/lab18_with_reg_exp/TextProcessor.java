4c3b14cf51b6c56262dc6a8b30275291668df4dd
dudinsky_a <dudinsky@tut.by>
2015-05-13 10:54:15 +0300

    Task18_with_regular_expression_version_2

--- Dudinsky/src/lab18_with_reg_exp/TextProcessor.java
+++ Dudinsky/src/lab18_with_reg_exp/TextProcessor.java
@@ -4,9 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextProcessor {
+    private final String WORD_COUNT_PATTERN = "[А-Яа-яЁёA-Za-z]+";
+
     public String catchLastLetters(String givenStr) {
         StringBuilder resultStrBuilder = new StringBuilder();
-        Pattern patternWord = Pattern.compile("[А-Яа-яЁёA-Za-z]+");
+        Pattern patternWord = Pattern.compile(WORD_COUNT_PATTERN);
         Matcher matcher = patternWord.matcher(givenStr);
         while (matcher.find()) {
             resultStrBuilder.append(givenStr.charAt(matcher.end() - 1));
