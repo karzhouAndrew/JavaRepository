700d778ffd866287e89acfdaa1939cf98fc78b33
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-14 13:11:22 +0300

    Lab20 Variable/method names corrected. Access specifiers added.

--- BatiushkovNikita/src/lab20/Lab20.java
+++ BatiushkovNikita/src/lab20/Lab20.java
@@ -7,17 +7,16 @@ import java.util.regex.Pattern;
 
 public class Lab20 {
 
-    final static String regEx = "0x\\p{XDigit}{1,}";
+    public final static String REG_EXP_HEX = "0x\\p{XDigit}{1,}";
 
     public static void main(String[] args) {
         String testString = "Aetu0x9A5F the inde0x12 this 0xF5 0xA7string0x59850xA0853AB0xGC56gur";
-        System.out.println(getRegExpModifiedString(testString, regEx));
+        System.out.println(getRegExpModifiedString(testString));
     }
 
-    private static StringBuilder getRegExpModifiedString(String testString, String regEx) {
-        Pattern pattern = Pattern.compile(regEx);
+    private static StringBuilder getRegExpModifiedString(String testString) {
+        Pattern pattern = Pattern.compile(REG_EXP_HEX);
         Matcher matcher = pattern.matcher(testString);
-
         StringBuilder modifiedString = new StringBuilder();
         while (matcher.find()) {
             modifiedString.append(matcher.group());
