1a4391253d0dd259fdd6d18f4bda78203d6a7346
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-14 17:01:44 +0300

    Task 18. Changed JCC pattern.

--- Boris/src/laba18/GetWordFromLastLetter.java
+++ Boris/src/laba18/GetWordFromLastLetter.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 //Вывод слова составленый из последних букв слов данной строки
 public class GetWordFromLastLetter {
-    private final static Pattern pattern = Pattern.compile("[a-zA-Zа-яА-ЯЁё]+");
+    private final static Pattern PATTERN_WORD = Pattern.compile("[a-zA-Zа-яА-ЯЁё]+");
 
     public static String getWord(String str) {
-        StringBuilder word = new StringBuilder("");
-        Matcher matcher = pattern.matcher(str);
+        StringBuilder word = new StringBuilder();
+        Matcher matcher = PATTERN_WORD.matcher(str);
         while (matcher.find()) {
             word.append(str.charAt(matcher.end() - 1));
         }
