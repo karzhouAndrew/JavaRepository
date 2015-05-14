0772338ff77cd7f5edddb9b7f8b40438f6f86a56
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-14 16:34:41 +0300

    Task 7. Changed value flag.

--- Boris/src/laba7/DifferentNumerals.java
+++ Boris/src/laba7/DifferentNumerals.java
@@ -5,19 +5,19 @@ public class DifferentNumerals {
     public static void main(String[] args) {
         int number = 1234584;
         int numberChange = number;
-        boolean flag = false;
+        boolean containSameNumbers = false;
         while (numberChange > 10) {
             int numeralLast = numberChange % 10;
             int numberRemainder = numberChange / 10;
             while (numberRemainder != 0) {
                 if (numeralLast == (numberRemainder % 10)) {
-                    flag = true;
+                    containSameNumbers = true;
                     break;
                 }
                 numberRemainder /= 10;
             }
             numberChange /= 10;
-            if (flag) {
+            if (containSameNumbers) {
                 System.out.println("Число " + number + " содержит одинаковые цифры");
                 break;
             } else if (numberChange < 10) {
