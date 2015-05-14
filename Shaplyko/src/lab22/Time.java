3b0600c2dcc3b63b43dfb695367f7a00cf954d11
Константин <k336699k@mail.ru>
2015-05-15 00:46:39 +0300

    Лабораторная работа 22

---
+++ Shaplyko/src/lab22/Time.java
@@ -0,0 +1,54 @@
+package lab22;
+
+/**
+ * Created by Константин on 14.05.2015.
+ */
+public class Time {
+    private int second;
+    private int minute;
+    private int hour;
+
+    public Time(int second) {
+        this.second = second;
+    }
+
+    public Time(int second, int minute, int hour) {
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+
+    public int getSecond() {
+        System.out.println("Количество секунд = " + second);
+        return second;
+    }
+
+    public int getMinute() {
+        System.out.println("Количество минут = " + minute);
+        return minute;
+    }
+
+    public int getHour() {
+        System.out.println("Количество часов = " + hour);
+        return hour;
+    }
+
+    public int amountSecond() {
+        return (hour * 60 * 60 + minute * 60 + second);
+    }
+
+    public int compareToTime(Object obj) {
+        Time tmp = (Time) obj;
+        if ((this.second + this.minute * 60 + this.hour * 60 * 60) < (tmp.second + tmp.minute * 60 + tmp.hour * 60 * 60)) {
+            System.out.println("Объекты 1 меньше объекта 2 = " + -1);
+            return -1;
+        } else if ((this.second + this.minute * 60 + this.hour * 60 * 60) > (tmp.second + tmp.minute * 60 + tmp.hour * 60 * 60)) {
+            System.out.println("Объекты 1 больше объекта 2 = " + 1);
+            return 1;
+        }
+        System.out.println("Объекты равны = " + 0);
+        return 0;
+    }
+}
+
+
