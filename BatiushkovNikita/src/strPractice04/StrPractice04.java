c1736d1bdf7b14a75643fe88301a8abacd5ea33c
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-14 14:02:26 +0300

    extraLab07

--- BatiushkovNikita/src/strPractice04/StrPractice04.java
+++ BatiushkovNikita/src/strPractice04/StrPractice04.java
@@ -6,10 +6,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StrPractice04 {
+
+    public final static int WORD_LENGTH = 4;
+
     public static void main(String[] args) {
         String text = "Жили-были мужик да баба. Оба были такие ленивые... Так и норовят дело на чужие плечи столкнуть, самим бы только не делать... И дверь-то в избу никогда на крюк не закладывали: утром-де вставай да руки протягивай, да опять крюк скидывай... И так проживем.\\n\" +\n" +
                 "                \"Вот раз баба и свари каши? А уж и каша сварилась! Румяна рассыпчата, крупина жжош от крупины так и отваливается?";
-        int wordLength = 4;
         getWorldsPredetermLength(text, wordLength);
     }
 
@@ -19,7 +21,6 @@ public class StrPractice04 {
         String[] sentenceArr;
         while (matcher.find()) {
             String sentence = matcher.group();
-            //System.out.println(sentence);
             if (getQuestionSentence(sentence) == true) {
                 sentence = getClearSentence(sentence);
                 sentenceArr = sentence.split(" +");
