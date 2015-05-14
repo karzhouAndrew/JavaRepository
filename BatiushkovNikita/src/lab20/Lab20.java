9a046c2538766ec56b4fedae79fad2b6851de88c
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-14 14:48:56 +0300

    Lab20 Constant change to "private"

--- BatiushkovNikita/src/lab20/Lab20.java
+++ BatiushkovNikita/src/lab20/Lab20.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class Lab20 {
 
-    public final static String REG_EXP_HEX = "0x\\p{XDigit}{1,}";
+    private final static String REG_EXP_HEX = "0x\\p{XDigit}{1,}";
 
     public static void main(String[] args) {
         String testString = "Aetu0x9A5F the inde0x12 this 0xF5 0xA7string0x59850xA0853AB0xGC56gur";
