7bbbe95513ebad45c99fa09da9549e09f89e96ed
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-14 08:28:07 +0300

    Lab07 rev.2

---
+++ BatiushkovNikita/src/lab07/Lab07.java
@@ -0,0 +1,24 @@
+package lab07;
+
+// Different digits in number.
+
+public class Lab07 {
+    public static void main(String[] args) {
+        int number = 120456780;
+        boolean answer = false;
+        while (number != 0) {
+            int testNum = number % 10;
+            int interimNum = number - number % 10;
+            interimNum /= 10;
+            while (interimNum != 0) {
+                if (testNum == interimNum % 10) {
+                    answer = true;
+                    break;
+                }
+                interimNum /= 10;
+            }
+            number /= 10;
+        }
+        System.out.println(answer);
+    }
+}
