c3e41cb50bf0a32bc447c8eb9a8109611488254b
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-14 17:06:20 +0300

    Task add 1. Changed value numberRemainder.

--- Boris/src/laba/add1/SumFractionNumerical.java
+++ Boris/src/laba/add1/SumFractionNumerical.java
@@ -6,10 +6,10 @@ public class SumFractionNumerical {
         double number = Math.random() + 3;
         int quantityNumber = 3;//(int) (Math.random() * 10 + 1);
         int sumNumber = 0;
-        double numberRemainder = number - (int) number;
+        double mantissaNumber = number - (int) number;
         for (int i = 0; i < quantityNumber; i++) {
-            numberRemainder *= 10;
-            sumNumber += ((int) (numberRemainder)) % 10;
+            mantissaNumber *= 10;
+            sumNumber += ((int) (mantissaNumber)) % 10;
         }
         System.out.println("Введенное число = " + number +
                 ". \nСумма " + quantityNumber + " чисел дробной части равна " + sumNumber);
