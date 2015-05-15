ecd18a83cd13299effa4cb0ba1e845406bacc872
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-16 00:59:39 +0300

    Lab22

--- BatiushkovNikita/src/lab22/Time.java
+++ BatiushkovNikita/src/lab22/Time.java
@@ -2,40 +2,46 @@ package lab22;
 
 public class Time {
     private int seconds;
-    private int minutes = 10;
+    private int minutes;
     private int hours;
 
-    // Конструктор
-    public Time(int seconds, int hours) {
-        this.seconds = seconds;
-        this.hours = hours;
+    //
+    public Time(int seconds, int minutes, int hours) {
+        timeInitializer(seconds, minutes, hours);
     }
 
-    public Time(int hours) {
-        this.hours = hours;
+    public Time(int seconds) {
+        timeInitializer(seconds);
     }
 
-    public int getSeconds() {
-        return seconds;
+
+    public int getAmountSeconds() {
+        return this.seconds + this.minutes * 60 + this.hours * 3600;
     }
 
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
+    public boolean compareTimeObjects(Time anotherTime) {
+        return getAmountSeconds() == anotherTime.getAmountSeconds();
     }
 
-    public int getMinutes() {
-        return minutes;
+    private void timeInitializer(int seconds, int minutes, int hours) {
+        this.seconds = seconds % 60;
+        this.minutes = (seconds / 60 + minutes) % 60;
+        //System.out.println(this.minutes);
+        this.hours = minutes / 60 + hours;
+        //System.out.println(this.hours);
+    }
+    private void timeInitializer(int seconds) {
+        this.seconds = seconds % 60;
+        int minutes =  (seconds - this.seconds) / 60;
+        this.minutes = ((seconds - this.seconds) / 60) % 60;
+        this.hours = ((minutes - this.minutes) / 60);
     }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
+ /*   int s = 6188392;
+    int sec = s % 60;
+    int m = (s - sec) / 60;
+    int min = m % 60;
+    int h = (m - min) / 60;
+    int hour = h % 24;
+    int d = (h - hour) / 24;*/
 }
