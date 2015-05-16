481a63756ca90bdeead1d670d1f1de3ab65c1c8f
Дмитрий <dimkaiv@list.ru>
2015-05-16 15:55:29 +0300

    This seventh task v.2

--- ivanchuk/src/task7/TaskSeven.java
+++ ivanchuk/src/task7/TaskSeven.java
@@ -8,35 +8,31 @@ public class TaskSeven {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите целое число.");
-        long number = in.nextLong();
+        int number = in.nextInt();
         in.close();
         int i = 0;
         while (number / Math.pow(10, i) >= 1) {
             i++;
         }
-        if (i > 10) {
-            System.out.println("Цифры числа совпадают.");
-        } else {
-            int[] array = new int[i];
-            for (int j = 0; j < array.length; j++) {
-                array[j] = (int) (number % Math.pow(10, j + 1) / Math.pow(10, j));
-            }
-            boolean test = true;
-            for (int j = 0; j < array.length; j++) {
-                for (int k = j + 1; k < array.length; k++) {
-                    if (array[j] == array[k]) {
-                        test = false;
-                        System.out.println("Цифры числа совпадают.");
-                        break;
-                    }
-                }
-                if (!test) {
+        int[] array = new int[i];
+        for (int j = 0; j < array.length; j++) {
+            array[j] = (int) (number % Math.pow(10, j + 1) / Math.pow(10, j));
+        }
+        boolean variousNumbers = true;
+        for (int j = 0; j < array.length; j++) {
+            for (int k = j + 1; k < array.length; k++) {
+                if (array[j] == array[k]) {
+                    variousNumbers = false;
+                    System.out.println("Цифры числа совпадают.");
                     break;
                 }
             }
-            if (test) {
-                System.out.println("Все цифры числа различны.");
+            if (!variousNumbers) {
+                break;
             }
+        }
+        if (variousNumbers) {
+            System.out.println("Все цифры числа различны.");
         }
     }
 }
