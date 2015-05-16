9066cd7cc9b83239ea2d470ad956d2422771935b
Константин <k336699k@mail.ru>
2015-05-16 13:49:31 +0300

    Лабораторная работа 16

--- Shaplyko/src/lab16/Lab16.java
+++ Shaplyko/src/lab16/Lab16.java
@@ -8,12 +8,6 @@ public class Lab16 {
     public static void main(String[] args) {
         Symbol symbol = new Symbol();
         String str = "Тестовая, строка, с несколькими,, - - ; ; ;; запятыми";
-        System.out.println("У нас есть " + symbol.comma(str) + " запятых");
-        System.out.println("У нас есть " + symbol.point(str) + " точек");
-        System.out.println("У нас есть " + symbol.colon(str) + " двоеточий");
-        System.out.println("У нас есть " + symbol.semicolon(str) + " точек с запятой");
-        System.out.println("У нас есть " + symbol.dash(str) + " тире");
-        int sum = symbol.dash(str) + symbol.semicolon(str) + symbol.colon(str) + symbol.point(str) + symbol.comma(str);
-        System.out.println("У нас всего знаков препинаний " + sum);
+        System.out.println("У нас всего знаков препинаний " + symbol.sumPunctuationMark(str));
     }
 }
--- Shaplyko/src/lab16/Symbol.java
+++ Shaplyko/src/lab16/Symbol.java
@@ -1,55 +1,14 @@
 package lab16;
 
 public class Symbol {
-    public int comma(String strEnter) {
-        int n = 0;
+    public int sumPunctuationMark(String strEnter) {
+        int sumPunctuationMark = 0;
         for (int i = 0; i < strEnter.length(); i++) {
             char symbol = strEnter.charAt(i);
-            if (symbol == ',') {
-                n++;
+            if (symbol == ',' || symbol == '.' || symbol == ':' || symbol == ';' || symbol == '-') {
+                sumPunctuationMark++;
             }
         }
-        return (n);
-    }
-    public int point(String strEnter) {
-        int n = 0;
-        for (int i = 0; i < strEnter.length(); i++) {
-            char symbol = strEnter.charAt(i);
-            if (symbol == '.') {
-                n++;
-            }
-        }
-        return (n);
-    }
-    public int colon(String strEnter) {
-        int n = 0;
-        for (int i = 0; i < strEnter.length(); i++) {
-            char symbol = strEnter.charAt(i);
-            if (symbol == ':') {
-                n++;
-            }
-        }
-        return (n);
-    }
-    public int semicolon(String strEnter) {
-        int n = 0;
-        for (int i = 0; i < strEnter.length(); i++) {
-            char symbol = strEnter.charAt(i);
-            if (symbol == ';') {
-                n++;
-            }
-        }
-        return (n);
-    }
-    public int dash(String strEnter) {
-        int n = 0;
-        for (int i = 0; i < strEnter.length(); i++) {
-            char symbol = strEnter.charAt(i);
-            if (symbol == '-') {
-                n++;
-            }
-        }
-        return (n);
+        return sumPunctuationMark;
     }
 }
-
