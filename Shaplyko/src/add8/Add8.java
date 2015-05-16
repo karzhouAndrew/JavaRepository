3e6186c719da4f8308dc7f21f33ebffbcb81a2b6
Константин <k336699k@mail.ru>
2015-05-16 14:10:58 +0300

    Дополнительное задание 8

--- Shaplyko/src/add8/Add8.java
+++ Shaplyko/src/add8/Add8.java
@@ -9,6 +9,6 @@ public class Add8 {
         String str = "child rbrbrt children rtbrbr grbr child rtbt 5456 nt 45646 child";
         Replace replace = new Replace();
         replace.childReplace(str);
-        replace.namberReplace(str);
+        replace.numberReplace(str);
     }
 }
--- Shaplyko/src/add8/Replace.java
+++ Shaplyko/src/add8/Replace.java
@@ -1,12 +1,10 @@
 package add8;
 
 public class Replace {
-
     public void childReplace(String str) {
         System.out.println(str.replaceAll("child", "children"));
     }
-
-    public void namberReplace(String str) {
+    public void numberReplace(String str) {
         System.out.println(str.replaceAll("\\d", ""));
     }
 }
