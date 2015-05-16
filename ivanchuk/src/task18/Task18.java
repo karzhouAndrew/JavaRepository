3d9d93bca30f4c7a81e4f256de193ac2efec206f
Дмитрий <dimkaiv@list.ru>
2015-05-16 19:45:52 +0300

    This eighteenth task.

--- ivanchuk/src/task18/Task18.java
+++ ivanchuk/src/task18/Task18.java
@@ -8,8 +8,8 @@ public class Task18 {
         StringBuilder lastLetters = new StringBuilder("");
         int lastPositionSearch = 0;
         for (int i = 0; i < textWithoutSpace.length(); i++) {
-            for (int j = 0; j < PUNCTUATION_MARK.length; j++) {
-                if (textWithoutSpace.charAt(i) == PUNCTUATION_MARK[j]) {
+            for (char punctuationMark : PUNCTUATION_MARK) {
+                if (textWithoutSpace.charAt(i) == punctuationMark) {
                     if (i - lastPositionSearch > 1) {
                         lastLetters.append(textWithoutSpace.charAt(i - 1));
                     }
