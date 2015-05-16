6c591803bc074f4b95a78bdbe3de432b873de8c7
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-16 20:17:14 +0300

    Удаление 16 и 18 задачи

--- Boris/src/laba18/TestGetWord.java
+++
@@ -1,9 +0,0 @@
-package laba18;
-
-//проверка класса GetWordFromLastLeter
-public class TestGetWord {
-    public static void main(String[] args) {
-        String strTest = "This text help us to check class GetWord.";
-        System.out.println(GetWordFromLastLetter.getWord(strTest));
-    }
-}
