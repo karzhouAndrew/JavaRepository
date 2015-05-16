8b4c56f5525475eb279a88c0f21ca1319f5c63b9
KirilenkoMV <Kirilenko_MV@bytechs.by>
2015-05-16 22:22:22 +0300

    создание иерархии классов(абстрактные классы, интерфейсы) задача про бытовую технику

--- KirilenkoMV/src/pwt/regulars/Ex21.java
+++ KirilenkoMV/src/pwt/regulars/Ex21.java
@@ -6,15 +6,16 @@ import java.util.regex.Pattern;
 /**
  * Created by Kirilenko_MV on 11.05.2015.
  * Найти в строке все абзацы, в том числе у которых есть параметры и заменить найденное на простой тэг <p>
- *
  */
 public class Ex21 {
     public static void main(String[] args) {
-        Pattern pattern =  Pattern.compile("(<p(.)+>)");//("<p(.)+>");
-        Matcher matcher = pattern.matcher("jhfbdf<p  jh>rfrf</p>mnb<p jhg>  gkjgf</p>nhvmg");
-        //matcher.replaceAll()
-        while(matcher.find()){
-            System.out.println("123 "+matcher.group());
+        String str = "Text <p  id='12'> Text </p> Text <p>Text</p>Text";
+        Pattern pattern = Pattern.compile("<p.*?>");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            System.out.println("Начало обзаца: " + matcher.group());
         }
+
+        System.out.println("Замена тэгов с параметрами: " + str.replaceAll("<p.*?>", "<p>"));
     }
 }
