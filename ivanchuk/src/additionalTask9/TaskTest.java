5491badbfe0c266c96fd86816fa05dbc5e6f61c0
Дмитрий <dimkaiv@list.ru>
2015-05-16 22:24:59 +0300

    This additional ninth task.

---
+++ ivanchuk/src/additionalTask9/TaskTest.java
@@ -0,0 +1,12 @@
+package additionalTask9;
+
+//Заменить в строке все вхождения 'child' на 'children'.
+//Удалить из текста все символы, являющиеся цифрами.
+
+public class TaskTest {
+    public static void main(String[] args) {
+        String text = "I 5656children7 chil9990 7867child67";
+        StringBuilder textEdited = new StringBuilder(AdditionalTask9.replaceChild(text));
+        System.out.println("Отредактированный текст: " + AdditionalTask9.deleteNumbers(textEdited));
+    }
+}
