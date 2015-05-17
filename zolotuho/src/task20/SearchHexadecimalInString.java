2850c9efbf3db0c4760e1030cfff335e1e6d3a89
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-18 00:15:52 +0300

    20th task Version2

--- zolotuho/src/task20/SearchHexadecimalInString.java
+++ zolotuho/src/task20/SearchHexadecimalInString.java
@@ -2,24 +2,23 @@ package task20;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 //Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
 // записаных по правилам Java, с помощью регулярных выражений и вывести их на страницу.
 public class SearchHexadecimalInString {
 
-    public int obtainEndPosition(StringBuilder stringBuilder, int startPosition) {
-        Pattern pattern = Pattern.compile("[0][x][0-9a-fA-F]{1,}");
-        Matcher matcher = pattern.matcher(stringBuilder);
-        matcher.find(startPosition);
-        return matcher.end();
-    }
-
-    public String obtainHexadecimalFromString(StringBuilder stringBuilder, int startPosition) {
-        Pattern pattern = Pattern.compile("[0][x][0-9a-fA-F]{1,}");
-        Matcher matcher = pattern.matcher(stringBuilder);
-        if (matcher.find(startPosition)) {
-            return matcher.group();
-        } else {
-            return null;
+    public String obtainHexadecimalFromString(String string) {
+        Pattern pattern = Pattern.compile("0x\\p{XDigit}+");
+        Matcher matcher = pattern.matcher(string);
+        StringBuilder stringBuilder = new StringBuilder("");
+        int position = 0;
+        int increment = 0;
+        while (matcher.find(position)) {
+            stringBuilder.append(matcher.group());
+            stringBuilder.append(' ');
+            increment++;
+            position = matcher.end();
         }
+        return stringBuilder.toString();
     }
 }
