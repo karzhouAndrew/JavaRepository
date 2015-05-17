eb757fd1112ecdf54cc7b68e74c148a5538aaae1
dudinsky <dudinsky@tut.by>
2015-05-17 07:26:27 +0300

    Additional_Task_03_version2

--- Dudinsky/src/addit03/AdditTask03.java
+++ Dudinsky/src/addit03/AdditTask03.java
@@ -4,10 +4,11 @@ package addit03;
 public class AdditTask03 {
     public static void main(String[] args) {
         int givenNumber = 100;
-        int intMax = Integer.MAX_VALUE;
-        System.out.println("Наибольшее значение, которое может поместиться в тип int: " + intMax);
-        double maxPower = Math.log10(intMax) / Math.log10(givenNumber);
-        System.out.println("Наибольшая степень числа " + givenNumber + ", которую можно вычислить, помещая результат в int, равняется: " + maxPower);
+        final int INT_MAX = Integer.MAX_VALUE;
+        System.out.println("Наибольшее значение, которое может поместиться в тип int: " + INT_MAX);
+        double maxPower = Math.log10(INT_MAX) / Math.log10(givenNumber);
+        System.out.println("ОТВЕТ: наибольшая целочисленная степень числа " + givenNumber + ", которую можно вычислить, помещая результат в int, равняется: " + (int) maxPower);
+        System.out.println("Наибольшая вещественная степень числа " + givenNumber + ", которую можно вычислить, помещая результат в int, равняется: " + maxPower);
         System.out.println("Проверка: " + givenNumber + " в степени " + maxPower + " равняется: " + (int) Math.pow(givenNumber, maxPower) + " (т.е. результат помещается в int)");
     }
 }
