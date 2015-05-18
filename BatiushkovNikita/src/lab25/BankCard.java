09707987e366cb8496397b5338b8e4bec0b14790
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-18 22:14:17 +0300

    lab25

---
+++ BatiushkovNikita/src/lab25/BankCard.java
@@ -0,0 +1,9 @@
+package lab25;
+
+public class BankCard {
+    private String bankName;
+    private String currency;
+    private String type;
+    private int value;
+    private boolean chipAvailability;
+}
---
+++ BatiushkovNikita/src/lab25/DebitBankCard.java
@@ -0,0 +1,5 @@
+package lab25;
+
+public class DebitBankCard extends BankCard {
+    protected boolean possibilityOfInternationalPayment;
+}
