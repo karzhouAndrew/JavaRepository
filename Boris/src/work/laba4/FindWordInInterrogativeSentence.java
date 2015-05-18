a732dea5e1e6a71cf9ff91debead2d27368a6de1
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-18 20:00:36 +0300

    String task 4.

---
+++ Boris/src/work/laba4/FindWordInInterrogativeSentence.java
@@ -0,0 +1,34 @@
+package work.laba4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//4.	Во всех вопросительных предложениях текста найти и напечатать слова заданной длины.
+public class FindWordInInterrogativeSentence {
+    private static final Pattern PATTERN_SENTENCE = Pattern.compile("[A-ZА-Я0-9][^.?!]+[.?!]+");
+
+    public static String getWords(String str, int sizeWord) {
+        Matcher matcher = PATTERN_SENTENCE.matcher(str);
+        StringBuilder wordsOut = new StringBuilder();
+        while (matcher.find()) {
+            String sentence = matcher.group();
+            if (sentence.endsWith("?")) {
+                wordsOut.append(findWords(sentence, sizeWord));
+            }
+        }
+        if (wordsOut.toString() == "") {
+            return "Слов заданной длины не найдено.";
+        }
+        return wordsOut.toString();
+    }
+
+    private static String findWords(String sentence, int sizeWord) {
+        StringBuilder getWord = new StringBuilder();
+        for (String word : sentence.split("[ \\p{Punct}]+")) {
+            if (word.length() == sizeWord) {
+                getWord.append(word).append(" ");
+            }
+        }
+        return getWord.toString();
+    }
+}
