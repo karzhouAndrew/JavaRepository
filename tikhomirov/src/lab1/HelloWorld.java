f12408f412a89c4ee121432cbc0d601cd5b45262
MaxTsikhamirau <m.tsikgamirau@gmail.com>
2015-05-18 20:13:36 +0300

    Task #1 HelloWorld

--- tikhomirov/src/lab1/HelloWorld.java
+++ tikhomirov/src/lab1/HelloWorld.java
@@ -1,10 +1,13 @@
 package lab1;
 
 /**
- * #1 Task
+ * #1 Task Вывести на экран "HelloWorld"
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("HelloWorld");
     }
 }
+
+
+
