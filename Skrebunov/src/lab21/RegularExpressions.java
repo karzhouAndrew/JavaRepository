6b78de3730330ce2598c10fa1a1da51816b5fddf
Skrebunov <skrebunov86@mail.ru>
2015-05-20 17:56:27 +0400

    doplab21

--- Skrebunov/src/lab21/RegularExpressions.java
+++ Skrebunov/src/lab21/RegularExpressions.java
@@ -1,8 +1,5 @@
 package lab21;
 
-/**
- * Created by PC on 20.05.2015.
- */
 public class RegularExpressions {
     public String replacingParagraphP(String textWithParagraphs) {
         String str = textWithParagraphs.replaceAll("<p.+?>", "<p>");
