21d175fdfef6bb1e64c051c51354ef8a203b8ec8
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-20 20:01:01 +0300

    Task 16

---
+++ Boris/src/laba16/FindPunctuations.java
@@ -0,0 +1,34 @@
+package laba16;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Дана строка. Найти и подсчитать все знаки препинания
+
+public class FindPunctuations {
+    private final static Pattern PATTERN_PUNCTUATION = Pattern.compile("\\p{Punct}");
+
+    public static int getQuantityPunctual(String str) {
+        int size = str.length();
+        int quantityPunctual = 0;
+        while (size > 0) {
+            char letter = str.charAt(str.length() - size);
+            if (letter == ',' || letter == '.' || letter == ':' || letter == ';' || letter == '!' || letter == '?' ||
+                    letter == '(' || letter == ')' || letter == '{' || letter == '}' || letter == '[' || letter == ']'
+                    || letter == '/' || letter == '\'') {
+                quantityPunctual++;
+            }
+            size--;
+        }
+        return quantityPunctual;
+    }
+
+    public static int getQuantityPunctualOther(String str) {
+        Matcher matcher = PATTERN_PUNCTUATION.matcher(str);
+        int quantityPunctual = 0;
+        while (matcher.find()) {
+            quantityPunctual++;
+        }
+        return quantityPunctual;
+    }
+}
