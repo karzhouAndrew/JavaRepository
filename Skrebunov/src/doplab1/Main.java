02419051c217e68773b86a91f6af5062a6729c26
Skrebunov <skrebunov86@mail.ru>
2015-05-20 17:17:01 +0400

    doplab1

---
+++ Skrebunov/src/doplab1/Main.java
@@ -0,0 +1,26 @@
+package doplab1;
+
+/**
+ * Created by PC on 20.05.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double number = 0.147;
+        double percent = number * 1000;
+        System.out.println(percent);
+        int sum = 0;
+        if (percent % 10000 == percent) {
+            sum = (int) ((percent % 10) + ((percent % 100) / 10) + ((percent % 1000) / 100));
+            System.out.println(sum);
+        } else if (percent % 1000 == percent) {
+            sum = (int) ((percent % 10) + ((percent % 100) / 10) + ((percent % 1000) / 100));
+            System.out.println(sum);
+        } else if (percent % 100 == percent) {
+            sum = (int) ((percent % 10) + ((percent % 100) / 10));
+            System.out.println(sum);
+        } else if (percent % 10 == percent) {
+            sum = (int) (percent % 10);
+            System.out.println(sum);
+        }
+    }
+}
