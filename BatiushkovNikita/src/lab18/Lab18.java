c5957d6e098c070fd10592113a53c3419168d23f
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-20 20:31:31 +0300

    Revert "Master"
    This reverts commit 9c94c7bcfabe91962f072bbf782a1a84c3821f5d.

--- BatiushkovNikita/src/lab18/Lab18.java
+++ BatiushkovNikita/src/lab18/Lab18.java
@@ -6,7 +6,7 @@ public class Lab18 {
     public static void main(String[] args) {
         String testString = "   thE StrinG! clasS.. representS,,, characteRdsds      ,,, ";
         testString = getClearSentence(testString);
-        System.out.println(getWordLastLetters(testString) + testString.charAt(testString.length() - 1));
+        System.out.println(getWordLastLetters(testString));
     }
 
     private static String getClearSentence(String str) {
@@ -14,13 +14,13 @@ public class Lab18 {
         return str.replaceAll("\\p{Punct}", "");
     }
 
-    private static String getWordLastLetters(String str) {
+    private static StringBuilder getWordLastLetters(String str) {
+        StringBuilder lastLetters = new StringBuilder();
         int strLength = str.length();
-        String lastLetters = "";
         for (int i = 0; i < strLength; i++) {
             char symbol = str.charAt(i);
             if (symbol == ' ') {
-                lastLetters += str.charAt(i - 1);
+                lastLetters.append(str.charAt(i - 1));
             }
         }
         return lastLetters;
