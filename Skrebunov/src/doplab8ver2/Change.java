21da5f51af7eb98a1df250fcc4940d32f0c4001e
Skrebunov <skrebunov86@mail.ru>
2015-05-21 23:18:42 +0400

    doplab8ver2

--- Skrebunov/src/doplab8ver2/Change.java
+++ Skrebunov/src/doplab8ver2/Change.java
@@ -1,6 +1,6 @@
 package doplab8ver2;
 
-
+//исправленная
 public class Change {
     public String changeChildren(String text) {
         String replaceChildren = text.replaceAll("Child", "Children");
