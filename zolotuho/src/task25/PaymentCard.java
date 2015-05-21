e11d307f04c7cf2fc419594f955088087a830cdd
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-21 17:32:23 +0300

    25th task

---
+++ zolotuho/src/task25/PaymentCard.java
@@ -0,0 +1,19 @@
+package task25;
+
+public class PaymentCard {
+    private final String bankName;
+    private final long cardID;
+
+    public PaymentCard(String bankName, long cardID) {
+        this.bankName = bankName;
+        this.cardID = cardID;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public long getCardID() {
+        return cardID;
+    }
+}
---
+++ zolotuho/src/task25/TestCards.java
@@ -0,0 +1,16 @@
+package task25;
+
+public class TestCards {
+    public static void main(String[] args) {
+        PaymentCard paymentCard = new MasterCard("Prior", 1234568343, 3000, 0, Currency.USD, 5);
+        PaymentCard paymentCard1 = new Visa("BPS", 900575889, 60000, 0, Currency.EU, 1);
+        Visa visa = new Visa("Idea", 578599474, 50000, 0, Currency.EU, 1);
+        PaymentCard paymentCard2;
+        paymentCard2 = visa;
+        ((Visa) paymentCard1).showBalance();
+        ((Visa) paymentCard2).showBalance();
+        ((Visa) paymentCard1).transferToAnotherCard(5000, paymentCard2);
+        ((Visa) paymentCard1).showBalance();
+        ((Visa) paymentCard2).showBalance();
+    }
+}
