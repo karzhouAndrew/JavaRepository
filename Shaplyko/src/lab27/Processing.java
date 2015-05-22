8e4fc47a849e0daafa9e2f5d3b4f6261996cb95b
Константин <k336699k@mail.ru>
2015-05-22 12:02:32 +0300

    Лабораторная работа 27

---
+++ Shaplyko/src/lab27/Processing.java
@@ -0,0 +1,26 @@
+package lab27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Processing {
+    List<Integer> listNumber = new ArrayList<Integer>();
+
+    public void generationNumber(int lengthList) {
+        for (int i = 0; i < lengthList; i++) {
+            listNumber.add(i, (int) (Math.random() * 10 + 1));
+        }
+        System.out.println("Список чисел " + listNumber);
+    }
+
+    public void processingNumber() {
+        for (int i = 0; i < listNumber.size(); i++) {
+            for (int j = 0; j < listNumber.size(); j++) {
+                if (listNumber.get(i) == listNumber.get(j) && i != j) {
+                    listNumber.remove(j);
+                }
+            }
+        }
+        System.out.println("Список чисел после редактирования " + listNumber);
+    }
+}
