aa68b5e1a1d38e35e9ff7e3978ed9ded4ee47fdc
Bronic86 <ezhik_b_tymane@mail.ru>
2015-05-23 20:36:00 +0300

    Task classwork 4. Regex constant.

--- Boris/src/work/laba4/FindWordInInterrogativeSentence.java
+++ Boris/src/work/laba4/FindWordInInterrogativeSentence.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 //4.	Во всех вопросительных предложениях текста найти и напечатать слова заданной длины.
 public class FindWordInInterrogativeSentence {
     private static final Pattern PATTERN_SENTENCE = Pattern.compile("[A-ZА-Я0-9][^.?!]+[.?!]+");
+    private static final String REGEX_FOR_SPLIT_WORD = "[ \\p{Punct}]+";
 
     public static String getWords(String str, int sizeWord) {
         Matcher matcher = PATTERN_SENTENCE.matcher(str);
@@ -16,7 +17,7 @@ public class FindWordInInterrogativeSentence {
                 wordsOut.append(findWords(sentence, sizeWord));
             }
         }
-        if (wordsOut.toString().equals("")) {
+        if (wordsOut.toString().length() == 0) {
             return "Слов заданной длины не найдено.";
         }
         return wordsOut.toString();
@@ -24,7 +25,7 @@ public class FindWordInInterrogativeSentence {
 
     private static String findWords(String sentence, int sizeWord) {
         StringBuilder getWord = new StringBuilder();
-        for (String word : sentence.split("[ \\p{Punct}]+")) {
+        for (String word : sentence.split(REGEX_FOR_SPLIT_WORD)) {
             if (word.length() == sizeWord) {
                 getWord.append(word).append(" ");
             }
