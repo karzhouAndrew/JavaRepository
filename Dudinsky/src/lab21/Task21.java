d3424535a82f1cef673680815649123267e37100
dudinsky <dudinsky@tut.by>
2015-05-23 19:42:18 +0300

    Task21_version2

--- Dudinsky/src/lab21/Task21.java
+++ Dudinsky/src/lab21/Task21.java
@@ -5,6 +5,6 @@ public class Task21 {
     public static void main(String[] args) {
         String givenStr = "<body><h1>Заголовок</h1><p id=\"p1\">Тестовая строка</p><p>содержащая</p><P id=\"p2\">разные теги</P></body>";
         TegProcessor tegProcessor = new TegProcessor();
-        System.out.println("В строке {\n" + givenStr + "\n} в тегах <p> и <P> удалены все параметры, ниже результат:\n" + tegProcessor.removeAllParamsOfPTegs(givenStr));
+        System.out.println("В строке {\n" + givenStr + "\n} в тегах <p> и <P> удалены все параметры, ниже результат:\n" + tegProcessor.removeAllPTegsParams(givenStr));
     }
 }
