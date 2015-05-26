c2eaeceb9fcfb452c9344bf96ff2a83a74fe1e21
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-26 22:00:16 +0300

    23d task Version2

--- zolotuho/src/task23/CashMachine.java
+++ zolotuho/src/task23/CashMachine.java
@@ -1,4 +1,5 @@
 package task23;
+
 //Создать класс и объекты описывающие Банкомат.
 // Набор купюр находящихся в банкомате должен задаваться тремя свойствами: количеством купюр номиналом 20 50 100.
 // Сделать методы для добавления денег в банкомат. Сделать функцию снимающую деньги.
@@ -6,59 +7,58 @@ package task23;
 // При снятии денег функция должна рапечатывать каким количеством купюр какого номинала выдается сумма.
 // Создать конструктор с тремя параметрами – количеством купюр. Прочее – на  ваше усмотрение.
 public class CashMachine {
-    private int numberOf20;
-    private int numberOf50;
-    private int numberOf100;
+    private int denomination20;
+    private int denomination50;
+    private int denomination100;
 
-    public CashMachine(int numberOf20, int numberOf50, int numberOf100) {
-        this.numberOf20 = numberOf20;
-        this.numberOf50 = numberOf50;
-        this.numberOf100 = numberOf100;
+    public CashMachine(int denomination20, int denomination50, int denomination100) {
+        this.denomination20 = denomination20;
+        this.denomination50 = denomination50;
+        this.denomination100 = denomination100;
+    }
+
+    public int getDenomination20() {
+        return denomination20;
+    }
+
+    public void setDenomination20(int denomination20) {
+        this.denomination20 = denomination20;
+    }
+
+    public int getDenomination50() {
+        return denomination50;
+    }
+
+    public void setDenomination50(int denomination50) {
+        this.denomination50 = denomination50;
+    }
+
+    public int getDenomination100() {
+        return denomination100;
+    }
+
+    public void setDenomination100(int denomination100) {
+        this.denomination100 = denomination100;
     }
 
     @Override
     public String toString() {
         return "CashMachine{" +
-                "numberOf20=" + numberOf20 +
-                ", numberOf50=" + numberOf50 +
-                ", numberOf100=" + numberOf100 +
+                "denomination20=" + denomination20 +
+                ", denomination50=" + denomination50 +
+                ", denomination100=" + denomination100 +
                 '}';
     }
 
-    public int getNumberOf20() {
-        return numberOf20;
-    }
-
-    public void setNumberOf20(int numberOf20) {
-        this.numberOf20 = numberOf20;
-    }
-
-    public int getNumberOf50() {
-        return numberOf50;
-    }
-
-    public void setNumberOf50(int numberOf50) {
-        this.numberOf50 = numberOf50;
-    }
-
-    public int getNumberOf100() {
-        return numberOf100;
-    }
-
-    public void setNumberOf100(int numberOf100) {
-        this.numberOf100 = numberOf100;
-    }
-
     public void addDenominations(int numberOf20, int numberOf50, int numberOf100) {
-        this.numberOf20 = this.numberOf20 + numberOf20;
-        this.numberOf50 = this.numberOf50 + numberOf50;
-        this.numberOf100 = this.numberOf100 + numberOf100;
-
+        this.denomination20 = this.denomination20 + numberOf20;
+        this.denomination50 = this.denomination50 + numberOf50;
+        this.denomination100 = this.denomination100 + numberOf100;
     }
 
-    public boolean obtainCash(int sum) {
+    public boolean withdrawCash(int sum) {
         int[] denomination = {100, 50, 20};
-        int[] quantity = {numberOf100, numberOf50, numberOf20};
+        int[] quantity = {denomination100, denomination50, denomination20};
         int numberDifferentDenomination = denomination.length;
         int[] countDenomination = new int[numberDifferentDenomination];
         int increment = 0;
@@ -69,22 +69,26 @@ public class CashMachine {
                 while (countDenomination[increment] < quantity[increment]) {
                     tempSum += denomination[increment];
                     countDenomination[increment]++;
-                    if (sum - tempSum < denomination[increment]) break;
+                    if (sum - tempSum < denomination[increment]) {
+                        break;
+                    }
                 }
-                if (tempSum == sum) break;
+                if (tempSum == sum) {
+                    break;
+                }
                 increment++;
             }
             for (int i = 0; i < countDenomination.length; i++) {
                 quantity[i] -= countDenomination[i];
                 switch (denomination[i]) {
                     case 100:
-                        numberOf100 = quantity[i];
+                        denomination100 = quantity[i];
                         break;
                     case 50:
-                        numberOf50 = quantity[i];
+                        denomination50 = quantity[i];
                         break;
                     case 20:
-                        numberOf20 = quantity[i];
+                        denomination20 = quantity[i];
                         break;
                 }
             }
