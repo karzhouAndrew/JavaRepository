ddc98920cc096e5298a92785724c60f01ddd2ea2
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-27 00:51:13 +0300

    additional task 3 Version2

--- zolotuho/src/additionalTask3/MaxPowerOfHundredInt.java
+++ zolotuho/src/additionalTask3/MaxPowerOfHundredInt.java
@@ -3,12 +3,12 @@ package additionalTask3;
 //	Определить, какую наибольшую степень числа 100 можно вычислить, пользуясь типом int.
 public class MaxPowerOfHundredInt {
     public static void main(String[] args) {
-        int n = 100;
-        long currentN = 100;
-        int counter = 0;
+        int number = 100;
+        long currentNumber = 100;
+        int counter = 1;
         System.out.println("Maximal value of integer = " + Integer.MAX_VALUE);
-        while (currentN <= Integer.MAX_VALUE) {
-            currentN *= n;
+        while (currentNumber <= Integer.MAX_VALUE / number) {
+            currentNumber *= number;
             counter++;
         }
         System.out.println("Max power of 100 = " + counter);
