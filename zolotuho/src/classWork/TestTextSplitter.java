e0967a117e8c6c928b87cf1f72741ef20489a352
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-26 13:01:01 +0300

    class work string task version2

--- zolotuho/src/classWork/TestTextSplitter.java
+++ zolotuho/src/classWork/TestTextSplitter.java
@@ -5,47 +5,35 @@ package classWork;
 public class TestTextSplitter {
     public static void main(String[] args) {
 
-        StringBuilder stringBuilder = new StringBuilder("i like an ice-cream. There is not any table! no? My car would be repaired if i had known.");
+        String string = "i like an ice-cream. There is not any table! no?   My car would be repaired if i had known.";
         TextSplitter textSplitter = new TextSplitter();
         System.out.println("Original text :");
-        System.out.println(stringBuilder);
-        int numberSentences = textSplitter.countSentences(stringBuilder);
-        StringBuilder[] sentence = new StringBuilder[numberSentences];
-
-        for (int i = 0; i < numberSentences; i++) {
-            sentence[i] = new StringBuilder("");
+        System.out.println(string);
+        int numberSentences = textSplitter.countSentences(string);
+        String[] sentence = textSplitter.obtainSentences(string);
+        int[] wordsNumber = new int[numberSentences];
+        for (int i = 0; i < wordsNumber.length; i++) {
+            wordsNumber[i] = textSplitter.countWords(sentence[i]);
         }
-
-        for (int i = 0; i < numberSentences; i++) {
-            textSplitter.obtainSentence(stringBuilder, sentence[i], i);
-        }
-
-        int[] numberOfWords = new int[numberSentences];
-        for (int i = 0; i < numberOfWords.length; i++) {
-            numberOfWords[i] = textSplitter.countWords(sentence[i]);
-        }
-
-        int[] indexOrder = new int[numberOfWords.length];
+        int[] indexOrder = new int[wordsNumber.length];
         for (int i = 0; i < indexOrder.length; i++) {
             indexOrder[i] = i;
         }
-
         int temp = 0;
-        for (int i = numberOfWords.length - 1; i >= 0; i--) {
+        for (int i = wordsNumber.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (numberOfWords[j] > numberOfWords[i]) {
-                    temp = numberOfWords[j];
-                    numberOfWords[j] = numberOfWords[i];
-                    numberOfWords[i] = temp;
+                if (wordsNumber[j] > wordsNumber[i]) {
+                    temp = wordsNumber[j];
+                    wordsNumber[j] = wordsNumber[i];
+                    wordsNumber[i] = temp;
                     temp = indexOrder[j];
                     indexOrder[j] = indexOrder[i];
                     indexOrder[i] = temp;
                 }
             }
         }
-
-        for (int i = 0; i < numberOfWords.length; i++) {
-            System.out.println(sentence[indexOrder[i]] + "  " + (numberOfWords[i]));
+        for (int i = 0; i < wordsNumber.length; i++) {
+            System.out.println(sentence[indexOrder[i]] + "  " + (wordsNumber[i]));
         }
     }
 }
--- zolotuho/src/classWork/TextSplitter.java
+++ zolotuho/src/classWork/TextSplitter.java
@@ -7,45 +7,55 @@ import java.util.regex.Pattern;
 //возрастания количества слов в каждом из них
 
 public class TextSplitter {
-    public int countSentences(StringBuilder stringBuilder) {
-        Pattern pattern = Pattern.compile("[.!?]");
-        Matcher matcher = pattern.matcher(stringBuilder);
-        int counterSentences = 0;
-        int currentPosition = 0;
-        while (matcher.find(currentPosition)) {
-            counterSentences++;
-            currentPosition = matcher.end();
-        }
-        return counterSentences;
+    public int countSentences(String string) {
+        String[] str = string.split("[.!?]");
+        return str.length;
     }
 
-    public void obtainSentence(StringBuilder stringBuilder, StringBuilder subStringBuilder, int index) {
+    public int countWords(String sentence) {
+        String[] str = sentence.split("[ ]");
+        return str.length;
+    }
+
+    public String obtainSentence(String string, int sentenceIndex) {
         Pattern pattern = Pattern.compile("[.!?]");
-        Matcher matcher = pattern.matcher(stringBuilder);
+        Matcher matcher = pattern.matcher(string);
+        StringBuilder stringBuilder = new StringBuilder();
         int currentPosition = 0;
         int iterator = 0;
         while (matcher.find(currentPosition)) {
-            if (index == iterator) {
-                subStringBuilder.append(stringBuilder.substring(currentPosition, matcher.end()));
-                while (subStringBuilder.charAt(0) == ' ') {
-                    subStringBuilder.deleteCharAt(0);
+            if (sentenceIndex == iterator) {
+                stringBuilder.append(string.substring(currentPosition, matcher.end()));
+                while (stringBuilder.charAt(0) == ' ') {
+                    stringBuilder.deleteCharAt(0);
                 }
             }
             iterator++;
             currentPosition = matcher.end();
         }
+        return stringBuilder.toString();
     }
 
-    public int countWords(StringBuilder sentence) {
-        int counterWords = 0;
-        int currentPosition = 0;
-        Pattern pattern = Pattern.compile("[ ]+");
-        Matcher matcher = pattern.matcher(sentence);
+    public String[] obtainSentences(String string) {
+        Pattern pattern = Pattern.compile("[.!?]");
+        Matcher matcher = pattern.matcher(string);
+        String[] auxiliaryString = string.split("[.!?]");
+        StringBuilder[] sentenceBuilder = new StringBuilder[auxiliaryString.length];
 
+        int currentPosition = 0;
+        int iterator = 0;
         while (matcher.find(currentPosition)) {
-            counterWords++;
+            sentenceBuilder[iterator] = new StringBuilder(string.substring(currentPosition, matcher.end()));
+            while (sentenceBuilder[iterator].charAt(0) == ' ') {
+                sentenceBuilder[iterator].deleteCharAt(0);
+            }
+            iterator++;
             currentPosition = matcher.end();
         }
-        return counterWords+1;
+        String[] str = new String[sentenceBuilder.length];
+        for (int i = 0; i < sentenceBuilder.length; i++) {
+            str[i] = sentenceBuilder[i].toString();
+        }
+        return str;
     }
 }
