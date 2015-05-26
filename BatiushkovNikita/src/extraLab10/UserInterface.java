49156768b28a8d778a82e1af5a1937364463f530
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-05-26 16:55:50 +0300

    ExtraLab10

--- BatiushkovNikita/src/extraLab10/UserInterface.java
+++ BatiushkovNikita/src/extraLab10/UserInterface.java
@@ -7,32 +7,59 @@ import java.util.Scanner;
 
 public class UserInterface {
     public static void main(String[] args) {
-        mainMenu();
+        //AutoHouse autoHouse = new AutoHouse();
+        menuAddCar();
+        //mainMenu();
     }
 
     public static void mainMenu() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please make a selection: ");
-
-        switch (scanner.nextInt()) {
-            case MainMenu.ADD_CAR.getValue():
-                System.out.println("1.  Add new car in AutoHouse.");
-                break;
-            case REMOVE_CAR:
-                System.out.println("2.  Remove car from the AutoHouse by ID.");
-                break;
-        }
-
-
-        /*System.out.println("1.  Add new car in AutoHouse.");
+        System.out.println("Welcome to the AutoHouse!");
+        System.out.println("1.  Add new car in AutoHouse.");
         System.out.println("2.  Remove car from the AutoHouse by ID.");
         System.out.println("3.  Search cars by brand.");
         System.out.println("4.  Search cars by year.");
         System.out.println("5.  Sort cars by year.");
         System.out.println("6.  Sort cars by price.");
         System.out.println("7.  View all cars.");
-        System.out.println();*/
+        System.out.println("8.  Exit.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("(Integer) Please make a selection: ");
+        if (scanner.hasNextInt()) {
+            int i = scanner.nextInt();
+            scanner.close();
+            switch (i) {
+                case 1:
+                    menuAddCar();
+                    break;
+                case 2:
+                    System.out.println("zzzzzzzzz");
+                    break;
+            }
+        } else {
+            System.out.println();
+            System.out.println("Wrong input. Try again." + "\n");
+            mainMenu();
+        }
     }
 
-
+    public static void menuAddCar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("(Integer) Enter car Id: ");
+        int carId;
+        while (scanner.hasNext() == true) {
+            if (scanner.hasNextInt()) {
+                carId = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("wrong");
+                //return;
+            }
+        }
+    }
 }
+
+
+
+
+
