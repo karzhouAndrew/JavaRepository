bdd8fd96c916c79316b11b29c4e9a8a3de96af87
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-27 21:22:45 +0300

    additional  task 2 Version2

--- zolotuho/src/additionalTask2/RealABCD.java
+++ zolotuho/src/additionalTask2/RealABCD.java
@@ -3,53 +3,40 @@ package additionalTask2;
 import java.util.Scanner;
 
 public class RealABCD {
-    public double enterReal(boolean bool, double number) {
-        if (bool) {
-            return number;
+
+    public double enterRealNumber(Scanner input, char symbol) {
+        System.out.print("Enter real " + symbol + " = ");
+        if (input.hasNextDouble()) {
+            return input.nextDouble();
         } else {
             return 0;
         }
     }
 
-    public void myException() {
-        System.err.println("Exception is occurred!! Incorrect number was possible entered");
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         RealABCD realNumber = new RealABCD();
-
-        try {
-
-            System.out.print("Enter real a = ");
-            double a = realNumber.enterReal(sc.hasNextDouble(), sc.nextDouble());
-            System.out.print("Enter real b = ");
-            double b = realNumber.enterReal(sc.hasNextDouble(), sc.nextDouble());
-            System.out.print("Enter real c = ");
-            double c = realNumber.enterReal(sc.hasNextDouble(), sc.nextDouble());
-            System.out.print("Enter real d = ");
-            double d = realNumber.enterReal(sc.hasNextDouble(), sc.nextDouble());
-            sc.close();
-
-            if ((a <= b) && (b <= c) && (c <= d)) {
-                a = d;
-                b = d;
-                c = d;
-                System.out.println();
-                System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
-            } else if ((a > b) && (b > c) && (c > d)) {
-                System.out.println();
-                System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
-            } else {
-                a = Math.pow(a, 2);
-                b = Math.pow(b, 2);
-                c = Math.pow(c, 2);
-                d = Math.pow(d, 2);
-                System.out.println();
-                System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
-            }
-        } catch (Exception e) {
-            realNumber.myException();
+        Scanner input = new Scanner(System.in);
+        double a = realNumber.enterRealNumber(input, 'a');
+        double b = realNumber.enterRealNumber(input, 'b');
+        double c = realNumber.enterRealNumber(input, 'c');
+        double d = realNumber.enterRealNumber(input, 'd');
+        input.close();
+        if ((a <= b) && (b <= c) && (c <= d)) {
+            a = d;
+            b = d;
+            c = d;
+            System.out.println();
+            System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
+        } else if ((a > b) && (b > c) && (c > d)) {
+            System.out.println();
+            System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
+        } else {
+            a = Math.pow(a, 2);
+            b = Math.pow(b, 2);
+            c = Math.pow(c, 2);
+            d = Math.pow(d, 2);
+            System.out.println();
+            System.out.println(" a = " + a + " b = " + b + " c = " + c + " d = " + d);
         }
     }
 }
