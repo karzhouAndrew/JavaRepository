ec6c6c7989efb89c8a3f4932f493dc9f1e8efdd2
Zolotuho Sergey <zoltanuga@gmail.com>
2015-05-28 07:27:50 +0300

    13th task Version2

--- zolotuho/src/task13/RotateMatrix.java
+++ zolotuho/src/task13/RotateMatrix.java
@@ -1,28 +1,32 @@
 package task13;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+
 //Создать квадратную матрицу, заполнить ее случайными элементами, распечатать, перевернуть,
 // и снова распечатать (при переворачивании нежелательно создавать еще один массив).
 public class RotateMatrix {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter dimension of the square matrix = ");
         if (sc.hasNext()) {
-            int n = sc.nextInt();
-            int[][] matriX = new int[n][n];
+            int matrixSize = sc.nextInt();
+            sc.close();
+            int[][] matriX = new int[matrixSize][matrixSize];
             Random rand = new Random();
             for (int i = 0; i < matriX.length; i++) {
                 for (int j = 0; j < matriX.length; j++) {
                     matriX[i][j] = rand.nextInt(100);
                 }
             }
-            for (int i = 0; i < matriX.length; i++) {
-                for (int j = 0; j < matriX.length; j++) {
-                    System.out.print(" " + matriX[i][j] + " ");
-                }
-                System.out.println();
+
+            for (int[] ints : matriX) {
+                System.out.println(Arrays.toString(ints));
             }
+
             for (int i = 0; i < matriX.length; i++) {
                 int temp = 0;
                 for (int j = i; j < matriX.length; j++) {
@@ -33,22 +37,20 @@ public class RotateMatrix {
             }
             for (int i = 0; i < matriX.length; i++) {
                 int temp = 0;
-                for (int j = 0; j < (int) matriX.length / 2; j++) {
+                for (int j = 0; j < matriX.length / 2; j++) {
                     temp = matriX[i][j];
                     matriX[i][j] = matriX[i][matriX.length - 1 - j];
                     matriX[i][matriX.length - 1 - j] = temp;
                 }
             }
+
             System.out.println("Rotated :");
-            for (int i = 0; i < matriX.length; i++) {
-                for (int j = 0; j < matriX.length; j++) {
-                    System.out.print(" " + matriX[i][j] + " ");
-                }
-                System.out.println();
+            for (int[] ints : matriX) {
+                System.out.println(Arrays.toString(ints));
             }
+
         } else {
             System.out.println("incorrect number");
         }
-        sc.close();
     }
 }
