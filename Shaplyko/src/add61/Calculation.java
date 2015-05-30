659ab2f460864d8561a47967d28b318eecbe0479
Константин <k336699k@mail.ru>
2015-05-31 00:09:34 +0300

    Дополнительное задание 6 один

--- Shaplyko/src/add61/Calculation.java
+++ Shaplyko/src/add61/Calculation.java
@@ -3,13 +3,13 @@ package add61;
 import static java.lang.Math.*;
 
 public class Calculation {
-    public double function1(int a, int b, int c) {
-        return (pow(sin(pow(a, b)), 2)) / (sqrt(1 + (pow(a, c))));
+    public double function1(int x, int t, int s) {
+        return (pow(sin(pow(x, t)), 2)) / (sqrt(1 + (pow(x, s))));
     }
 
-    public double function2(int a) {
-        double numerator = (pow((cos(pow(E, a)) + pow(E, pow(a, 2)) + sqrt(1 / a)), 0.25));
-        double denominator = (pow(cos(PI * pow(a, 3)) + log10(pow(1 + a, 2)), sin(a)));
+    public double function2(int x) {
+        double numerator = (pow((cos(pow(E, x)) + pow(E, pow(x, 2)) + sqrt(1 / x)), 0.25));
+        double denominator = (pow(cos(PI * pow(x, 3)) + log10(pow(1 + x, 2)), sin(x)));
         return numerator / denominator;
     }
 }
