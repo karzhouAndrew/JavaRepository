a8d0ef293afed89f98831be13d046652678fffa3
Константин <k336699k@mail.ru>
2015-06-01 13:11:21 +0300

    Лабораторная работа 28

--- Shaplyko/src/lab28/Processing.java
+++ Shaplyko/src/lab28/Processing.java
@@ -5,20 +5,20 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class Processing {
-    List<Integer> listMark = new ArrayList<Integer>();
+  private List<Student> listMark = new ArrayList<Student>();
 
     public void generationMark(int lengthList) {
         for (int i = 0; i < lengthList; i++) {
-            listMark.add(i, (int) (Math.random() * 10 + 1));
+            listMark.add(i, new Student("Ученик" + i, (int) (Math.random() * 10 + 1)));
         }
         System.out.println("Список оценок " + listMark);
     }
 
     public void processingMark() {
-        ListIterator<Integer> iterator = listMark.listIterator();
-        int maxMark = listMark.get(0);
+        ListIterator<Student> iterator = listMark.listIterator();
+       int maxMark = listMark.get(0).getMark();
         while (iterator.hasNext()) {
-            int temp = iterator.next();
+            int temp = iterator.next().getMark();
             if (temp > maxMark) {
                 maxMark = temp;
             }
