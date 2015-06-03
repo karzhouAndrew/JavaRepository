75287cefeebcf9cafbe4f50d9c7e0e5b30a270dd
Дмитрий <dimkaiv@list.ru>
2015-06-03 14:06:09 +0300

    This annotation task version 2

--- ivanchuk/src/annotationTask/TransactionAnalyzer.java
+++ ivanchuk/src/annotationTask/TransactionAnalyzer.java
@@ -1,6 +1,5 @@
 package annotationTask;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -10,22 +9,20 @@ public class TransactionAnalyzer {
         Class<?> clazz = object.getClass();
         Method[] methods = clazz.getMethods();
         for (Method method : methods) {
-            Annotation[] annotations = method.getAnnotations();
-            for (Annotation annotation : annotations) {
-                if (annotation.toString().equals("@annotationTask.Transaction()")) {
-                    System.out.println("Transaction is started");
-                    try {
-                        method.invoke(object, null);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Transaction is ended");
+            if (method.isAnnotationPresent(Transaction.class)) {
+                System.out.println("Transaction is started");
+                try {
+                    method.invoke(object, null);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
                 }
+                System.out.println("Transaction is ended");
             }
         }
     }
 }
+
 
 
