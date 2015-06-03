52b26de701a88ba1ee5c77dc499c30b450324e60
Дмитрий <dimkaiv@list.ru>
2015-06-03 16:15:57 +0300

    This additional nine task version 2.

--- ivanchuk/src/additionalTask9/AdditionalTask9.java
+++ ivanchuk/src/additionalTask9/AdditionalTask9.java
@@ -1,8 +1,5 @@
 package additionalTask9;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class AdditionalTask9 {
     private static final String NUMBERS_PATTERN = "\\d+";
 
@@ -10,16 +7,8 @@ public class AdditionalTask9 {
         return text.replaceAll("child", "children");
     }
 
-    public static StringBuilder deleteNumbers(String text) {
-        StringBuilder textWithoutNumbers = new StringBuilder(text);
-        Pattern pattern = Pattern.compile(NUMBERS_PATTERN);
-        Matcher matcher = pattern.matcher(text);
-        int numbersQuantity = 0;
-        while (matcher.find()) {
-            textWithoutNumbers.delete(matcher.start() - numbersQuantity, matcher.end() - numbersQuantity);
-            numbersQuantity += matcher.end() - matcher.start();
-        }
-        return textWithoutNumbers;
+    public static String deleteNumbers(String text) {
+        return text.replaceAll(NUMBERS_PATTERN, "");
     }
 }
 
