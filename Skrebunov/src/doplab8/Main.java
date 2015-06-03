8c872dc577057204b605c3d531afefff6067f451
Skrebunov <skrebunov86@mail.ru>
2015-06-03 13:12:30 +0400

    doplab8

---
+++ Skrebunov/src/doplab8/Main.java
@@ -0,0 +1,11 @@
+package doplab8;
+
+/*Заменить в строке все вхождения 'child' на 'children'.  Удалить из текста все символы, являющиеся цифрами.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Change object = new Change();
+        String result = object.changeChildren("12 Child, 1783413 Children jsjsdsjdh 77 jjjj 098? Rtvjj 1, - Child");
+        System.out.println(result);
+    }
+}
