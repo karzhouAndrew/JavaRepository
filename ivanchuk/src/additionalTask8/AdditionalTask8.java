d1887583d8a6ffc4efcd6d686fb9f8de862d43ee
Дмитрий <dimkaiv@list.ru>
2015-06-03 12:24:30 +0300

    This additional eighth task.

--- ivanchuk/src/additionalTask8/AdditionalTask8.java
+++ ivanchuk/src/additionalTask8/AdditionalTask8.java
@@ -4,9 +4,9 @@ public class AdditionalTask8 {
     private static final char[] CONSONANT = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'};
     private static final char[] VOWELS = {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р',
             'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
-    private static final String PATTERN_OFFERS = "[!?\\.]+ ";
+    private static final String OFFERS_PATTERN = "[!?\\.]+ ";
 
-    public static int consonantQuantity(String text) {
+    public static int calcConsonantQuantity(String text) {
         int quantity = 0;
         String textLowerCase = text.toLowerCase();
         for (int i = 0; i < textLowerCase.length(); i++) {
@@ -20,7 +20,7 @@ public class AdditionalTask8 {
         return quantity;
     }
 
-    public static int vowelsQuantity(String text) {
+    public static int calcVowelsQuantity(String text) {
         int quantity = 0;
         String textLowerCase = text.toLowerCase();
         for (int i = 0; i < textLowerCase.length(); i++) {
@@ -34,17 +34,17 @@ public class AdditionalTask8 {
         return quantity;
     }
 
-    public static StringBuilder[] offersSearch(String text) {
-        String[] offers = text.split(PATTERN_OFFERS);
+    public static StringBuilder[] findOffers(String text) {
+        String[] offers = text.split(OFFERS_PATTERN);
         StringBuilder[] newOffers = new StringBuilder[offers.length];
-        for (int i = 0; i < offers.length; i++) {
+        for (int i = 0; i < newOffers.length; i++) {
             newOffers[i] = new StringBuilder(offers[i].replaceAll("\n", ""));
             newOffers[i].append(" - ").append(newOffers[i].length()).append(" символов.");
         }
         return newOffers;
     }
 
-    public static int spaceQuantity(String text) {
+    public static int calcSpaceQuantity(String text) {
         int quantity = 0;
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) == ' ') {
@@ -54,7 +54,7 @@ public class AdditionalTask8 {
         return quantity;
     }
 
-    public static int paragraphsQuantity(String text) {
+    public static int calcParagraphsQuantity(String text) {
         int quantity = 1;
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) == '\n') {
@@ -64,7 +64,7 @@ public class AdditionalTask8 {
         return quantity;
     }
 
-    public static String textWithoutSpace(String text) {
+    public static String getTextWithoutSpace(String text) {
         return text.replaceAll(" +", "");
     }
 }
--- ivanchuk/src/additionalTask8/TaskTest.java
+++ ivanchuk/src/additionalTask8/TaskTest.java
@@ -3,7 +3,7 @@ package additionalTask8;
 //Взять любой русскоязычный̆ текст, в котором не меньше трёхсот знаков.
 //Необходимо провести анализ этого текста на:
 //        a)	   Количество согласных и гласных букв.  
-//        b)	   Вывести каждое предложение этого текста в отдельной̆ строке с указанием количества символов в нём.  
+//        b)	   Вывести каждое предложение этого текста в отдельной строке с указанием количества символов в нём.  
 //        c)	   Посчитать количество пробелов и абзацев (абзацем считается переход на новую строку).  
 //        d)	   Создать текст, в котором будут убраны все пробельные символы в исходном тексте, и вывести его.  
 
@@ -15,14 +15,15 @@ public class TaskTest {
                 "США остался бы в памяти белорусских болельщиков... Перед тем, как встретится \n" +
                 "с Россией, беларусы сотворили одну из главных сенсаций чемпионата мира! \n" +
                 "Встретившись с американцами, они не просто обыграли звёздно-полосатых, а сделали это красиво.";
-        System.out.println("Количество гласных: " + AdditionalTask8.consonantQuantity(text));
-        System.out.println("Количество согласных: " + AdditionalTask8.vowelsQuantity(text));
-        StringBuilder[] arrayOffers = AdditionalTask8.offersSearch(text);
+        System.out.println(text);
+        System.out.println("Количество гласных: " + AdditionalTask8.calcConsonantQuantity(text));
+        System.out.println("Количество согласных: " + AdditionalTask8.calcVowelsQuantity(text));
+        StringBuilder[] arrayOffers = AdditionalTask8.findOffers(text);
         for (StringBuilder offers : arrayOffers) {
             System.out.println(offers);
         }
-        System.out.println("Количество пробелов: " + AdditionalTask8.spaceQuantity(text));
-        System.out.println("Количество абзацев: " + AdditionalTask8.paragraphsQuantity(text));
-        System.out.println(AdditionalTask8.textWithoutSpace(text));
+        System.out.println("Количество пробелов: " + AdditionalTask8.calcSpaceQuantity(text));
+        System.out.println("Количество абзацев: " + AdditionalTask8.calcParagraphsQuantity(text));
+        System.out.println(AdditionalTask8.getTextWithoutSpace(text));
     }
 }
