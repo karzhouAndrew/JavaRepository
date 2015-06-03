083d70fe9525d8608647e175295dbb78b385ad1c
Дмитрий <dimkaiv@list.ru>
2015-06-03 09:59:09 +0300

    This seventeenth task version 2.

--- ivanchuk/src/task17/Task17.java
+++ ivanchuk/src/task17/Task17.java
@@ -4,16 +4,15 @@ public class Task17 {
     public static int wordsQuantity(String text) {
         String textWithoutSpace = text.trim();
         int quantity = 1;
-        int currentPositionOfSearch = 0;
-        int lastPositionOfSearch;
-        while (currentPositionOfSearch != -1) {
-            lastPositionOfSearch = currentPositionOfSearch;
-            currentPositionOfSearch = textWithoutSpace.indexOf(' ', lastPositionOfSearch);
-            if (currentPositionOfSearch != -1) {
-                if (currentPositionOfSearch - lastPositionOfSearch > 0) {
+        int currentSearchPosition = 0;
+        while (currentSearchPosition != -1) {
+            int lastSearchPosition = currentSearchPosition;
+            currentSearchPosition = textWithoutSpace.indexOf(' ', lastSearchPosition);
+            if (currentSearchPosition != -1) {
+                if (currentSearchPosition - lastSearchPosition > 0) {
                     quantity++;
                 }
-                currentPositionOfSearch++;
+                currentSearchPosition++;
             }
         }
         return quantity;
