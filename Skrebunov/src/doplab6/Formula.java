ed5648cba9aa9ce3e500debc6a9ec665c898732d
Skrebunov <skrebunov86@mail.ru>
2015-06-03 14:02:34 +0400

    doplab6

--- Skrebunov/src/doplab6/Formula.java
+++ Skrebunov/src/doplab6/Formula.java
@@ -1,31 +1,18 @@
 package doplab6;
 
 import static java.lang.Math.*;
-import static java.lang.Math.pow;
 
 
 public class Formula {
     public double firstFormula(int x, int n) {
-        double squareRootLast = sqrt(1 / x);
-        int degreeA = (int) pow(x, 2);
-        double constantE = pow(E, degreeA);
-        double degreeFromA = pow(E, x);
-        double cosFromE = cos(degreeFromA);
-        double squareRootFirst = sqrt(squareRootLast + constantE + cosFromE);
-        double sqrtFromUp = pow(squareRootFirst, 1.0 / 3);
-        int degreeABot = (int) pow(x, 3);
-        double piBot = PI * degreeABot;
-        double cosBot = cos(piBot);
-        double bracketsBot = pow(x + 1, 2) + n + cosBot;
-        double sinX = sin(x);
-        double powOfBracketsBot = pow(bracketsBot, sinX);
-        return sqrtFromUp / powOfBracketsBot;
+        double numerator = pow(sqrt(cos(pow(E, x)) + pow(E, pow(x, 2)) + sqrt(1. / x)), 1.0 / 4);
+        double denominator = pow(cos(pow(x, 3)) + (pow(x + 1, 2) * n), sin(x));
+        return numerator / denominator;
     }
 
     public double secondFormula(int x, int t, int s) {
-        double squareRootA = pow(x, t);
-        double sinUp = ((cos(2 * squareRootA)) - 1) / 2;
-        double sqrtBot = sqrt((pow(x, s) + 1));
-        return sinUp / sqrtBot;
+        double numerator = ((cos(2 * pow(x, t))) - 1) / 2;
+        double denominator = sqrt((pow(x, s) + 1));
+        return numerator / denominator;
     }
 }
