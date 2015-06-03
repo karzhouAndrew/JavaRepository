9c4f7772b8a971ad5f057ec42610b069302e9a40
Skrebunov <skrebunov86@mail.ru>
2015-06-03 12:54:35 +0400

    doplab8ver2

--- Skrebunov/src/lab20/RegularExpressions.java
+++ Skrebunov/src/lab20/RegularExpressions.java
@@ -4,10 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpressions {
+    private final Pattern PATTERN = Pattern.compile("-?[xX][0-9a-fA-F]+");
+
     public String regular(String textWithHex) {
         StringBuilder str = new StringBuilder("Hexadecimal numbers - ");
-        Pattern pattern = Pattern.compile("-?[xX][0-9a-fA-F]+");
-        Matcher matcher = pattern.matcher(textWithHex);
+        Matcher matcher = PATTERN.matcher(textWithHex);
         while (matcher.find()) {
             str.append(matcher.group());
             str.append(" ");
