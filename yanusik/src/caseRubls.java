df81f010accbae7024f13d74bcf4bb59cf79f19f
shadow5551 <yanusikdima@mail.ru>
2015-06-05 02:07:02 +0300

    Update caseRubls.java

--- yanusik/src/caseRubls.java
+++ yanusik/src/caseRubls.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class caseRubls {
+public class CaseRubls {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int value = in.nextInt();
