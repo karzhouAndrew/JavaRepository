e7183f5806d8b538378880dbe9938c07ab33b114
dmitry yanusik <yanusikdima@mail.ru>
2015-06-05 01:43:37 +0300

    lab16

---
+++ yanusik/src/caseRubls.java
@@ -0,0 +1,43 @@
+/**
+ * Имеется целое число (любое), это число — сумма денег в рублях.
+ * Вывести это число, добавив к нему слово «рублей» в правильном падеже.
+ */
+
+import java.util.Scanner;
+
+public class caseRubls {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int value = in.nextInt();
+        int val = value % 100;
+        switch (val) {
+            case 0:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+                System.out.printf(value + " " + "рублей");
+                break;
+            case 1:
+                System.out.printf(value + " " + "рубль");
+                break;
+
+            case 2:
+            case 3:
+            case 4:
+                System.out.printf(value + " " + "рубля");
+                break;
+        }
+    }
+}
