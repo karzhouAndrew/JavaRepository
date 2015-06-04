1a22ce93803ba79b206060e4b1366d5f008b94ba
dmitry yanusik <yanusikdima@mail.ru>
2015-06-05 02:23:53 +0300

    lab8

---
+++ yanusik/src/Factorial.java
@@ -0,0 +1,20 @@
+/**
+ * Изменить  пример  с  суммой  чисел  таким  образом,  чтобы
+ * рассчитывалась не сумма, а произведение, т.е. факто
+ * риал числа.
+ */
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("enter the value ");
+        int value = in.nextInt();
+        long factorial = 1;
+        for (int i = 1; i <= value; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+    }
+}
