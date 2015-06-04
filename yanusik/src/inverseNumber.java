70fe0c7880ed6935a8bfe08273e28f6a9eccb3c7
shadow5551 <yanusikdima@mail.ru>
2015-06-05 02:07:53 +0300

    Update inverseNumber.java

--- yanusik/src/inverseNumber.java
+++ yanusik/src/inverseNumber.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class inverseNumber {
+public class InverseNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int value = in.nextInt();
