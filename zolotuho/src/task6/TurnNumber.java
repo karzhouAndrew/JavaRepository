8caca8d25b84cb6cd015b22218fec485bd632e29
Zolotuho Sergey <zoltanuga@gmail.com>
2015-06-07 23:30:03 +0300

    6th task Version2

--- zolotuho/src/task6/TurnNumber.java
+++ zolotuho/src/task6/TurnNumber.java
@@ -7,27 +7,19 @@ public class TurnNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Еnter number :");
-        int quantityDigits = 0;
+        int number = 0;
         if (sc.hasNextInt()) {
-            int number = sc.nextInt();
-            int numberAuxiliary = number;
-            while (numberAuxiliary >= 1) {
-                numberAuxiliary /= 10;
-                quantityDigits++;
-            }
-            int[] digits = new int[quantityDigits];
-            numberAuxiliary = number;
-            for (int i = 0; i < digits.length; i++) {
-                digits[i] = (numberAuxiliary % 10);
-                numberAuxiliary = numberAuxiliary - digits[i];
-                numberAuxiliary /= 10;
-            }
-            for (int digit : digits) {
-                System.out.print(digit);
-            }
+            number = sc.nextInt();
         } else {
             System.out.println("Incorrect number was entered");
         }
         sc.close();
+         int auxiliaryNumber = number;
+        while (auxiliaryNumber >= 1) {
+            int digit = (auxiliaryNumber % 10);
+            auxiliaryNumber = auxiliaryNumber - digit;
+            System.out.print(digit);
+            auxiliaryNumber /= 10;
+        }
     }
 }
