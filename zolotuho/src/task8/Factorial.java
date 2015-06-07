8c37c0e44b259ce3e636b2abf819dfb2aa20fe3a
Zolotuho Sergey <zoltanuga@gmail.com>
2015-06-08 00:01:34 +0300

    8th task Version2

--- zolotuho/src/task8/Factorial.java
+++ zolotuho/src/task8/Factorial.java
@@ -1,32 +1,27 @@
 package task8;
 
-//Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а произведение, т.е. факториал числа.
-
 import java.util.Scanner;
 
+//Изменить пример с суммой чисел таким образом, чтобы рассчитывалась не сумма, а произведение, т.е. факториал числа.
 public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number: ");
+        int number = 0;
         if (sc.hasNextInt()) {
-            int number = sc.nextInt();
-            long factorial = 1;
-            if (number != 0) {
-                for (int i = 1; i <= number; i++) {
-                    if (factorial >= Long.MAX_VALUE / number + 1) {
-                        System.err.println("Out of Long Type boundaries occurred");
-                        System.out.println("Current iteration = " + i);
-                        break;
-                    }
-                    factorial *= i;
-                }
-            } else {
-                factorial = 0;
-            }
-            System.out.println("Factorial = " + factorial);
-        } else {
-            System.out.println("Incorrect number is entered");
+            number = sc.nextInt();
         }
         sc.close();
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            if (factorial > Long.MAX_VALUE / number) {
+                System.err.println("Out of Long Type boundaries occurred");
+                System.out.println("Current iteration = " + i);
+                System.out.println("Current");
+                break;
+            }
+            factorial *= i;
+        }
+        System.out.println("Factorial = " + factorial);
     }
 }
