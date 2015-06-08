f21d52459ec3d68beeabc0a75b3a67b8d7c0ac62
Zolotuho Sergey <zoltanuga@gmail.com>
2015-06-09 00:34:34 +0300

    15th task Version3

--- zolotuho/src/task15/ButterflyMatrix.java
+++ zolotuho/src/task15/ButterflyMatrix.java
@@ -11,16 +11,10 @@ public class ButterflyMatrix {
             int matrixSize = sc.nextInt();
             sc.close();
             int[][] matrix = new int[matrixSize][matrixSize];
-            for (int i = 0; i < matrix.length; i++) {
-                int decrement = matrix.length - 1 - i;
-                if (i <= matrix.length / 2) {
-                    for (int j = i; j < matrix.length - i; j++) {
-                        matrix[i][j] = 1;
-                    }
-                } else {
-                    for (int j = decrement; j < matrix.length - decrement; j++) {
-                        matrix[i][j] = 1;
-                    }
+            for (int i = 0; i < matrix.length / 2 + 1; i++) {
+                for (int j = i; j < matrix.length - i; j++) {
+                    matrix[i][j] = 1;
+                    matrix[matrix.length - i - 1][j] = 1;
                 }
             }
             for (int[] ints : matrix) {
