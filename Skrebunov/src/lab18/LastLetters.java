0084f85c9a8acfc8f031a099f73aa7e996343686
Skrebunov <skrebunov86@mail.ru>
2015-06-08 16:05:56 +0400

    lab18

---
+++ Skrebunov/src/lab18/LastLetters.java
@@ -0,0 +1,18 @@
+package lab18;
+
+
+public class LastLetters {
+    public void lastLetterToWord(String text) {
+        char symbol;
+        for (int i = 0; i < text.length(); i++) {
+            symbol = text.charAt(i);
+            if (symbol != ' ' && symbol != '?' && symbol != '!' && symbol != ',' && symbol != '.') {
+                if (i == text.length() - 1) {
+                } else if (text.charAt(i + 1) == ' ' || text.charAt(i + 1) == '?' || text.charAt(i + 1) == '!' || text.charAt(i + 1) == ',' || text.charAt(i + 1) == '.') {
+                    System.out.print(symbol);
+                }
+            }
+        }
+    }
+}
+
