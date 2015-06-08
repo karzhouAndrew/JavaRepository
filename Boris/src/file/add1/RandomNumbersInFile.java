9c52e7b11c54746dd02830b2d031a72d93a9747e
Bronic86 <ezhik_b_tymane@mail.ru>
2015-06-08 23:04:32 +0300

    Files add1

--- Boris/src/file/add1/RandomNumbersInFile.java
+++ Boris/src/file/add1/RandomNumbersInFile.java
@@ -4,33 +4,69 @@ package file.add1;
 // Отсортировать содержимое файла по возрастанию.
 
 
-import java.io.File;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class RandomNumbersInFile {
     private File path;
+    private String fileName;
+    private Random rand = new Random();
 
     public RandomNumbersInFile() {
-        this(new File("../Test", "test"));
+        this("../Test", "test.txt");
     }
 
     public RandomNumbersInFile(String path, String name) {
         this.path = new File(path);
+        fileName = name;
         this.path.mkdirs();
-        create(name);
     }
 
-    private void create(String name) {
-        try (OutputStreamWriter write = new OutputStreamWriter(name)){
-
+    public void writeRandomNumberToFile(int quantity){
+        try(DataOutputStream writer = new DataOutputStream(new BufferedOutputStream( new FileOutputStream(path + "\\" + fileName)))) {
+            addNumbersToFile(writer, quantity);
+        } catch (IOException e) {
+            System.out.println("Exception opened.");
         }
     }
 
-    public void deleteFile(){
-    for (File fileName : path.listFiles()) {
-        fileName.delete();
+    private void addNumbersToFile(DataOutputStream writer, int quantity) throws IOException {
+        for (int i = 0; i < quantity; i++) {
+            writer.write(getRandomNumber());
+        }
     }
-}
+
+    private int getRandomNumber() {
+        return 10; //rand.nextInt(100);
+    }
+
+    public List<Integer> getListNumbersInFile (){
+        List<Integer> numbers = new ArrayList<>();
+        try (DataInputStream reader = new DataInputStream( new BufferedInputStream(new FileInputStream(path.getAbsoluteFile() + "\\" + fileName)))){
+            addNumber(numbers, reader);
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception file not found.");
+        } catch (IOException e) {
+            System.out.println("Exception opened file.");;
+        }
+        return numbers;
+    }
+
+    private void addNumber(List<Integer> numbers, DataInputStream reader) throws IOException {
+        Integer number = reader.readInt();
+        while (number != null){
+            numbers.add(number);
+            number = reader.readInt();
+        }
+    }
+
+    public void deleteFile() {
+        for (File fileName : path.listFiles()) {
+            fileName.delete();
+        }
+    }
 
 }
--- Boris/src/file/add1/TestRandomNumbersInFile.java
+++ Boris/src/file/add1/TestRandomNumbersInFile.java
@@ -6,6 +6,8 @@ public class TestRandomNumbersInFile {
     public static void main(String[] args) {
 
         RandomNumbersInFile file = new RandomNumbersInFile();
+        file.writeRandomNumberToFile(10);
+        System.out.println(file.getListNumbersInFile());
 
     }
 }
