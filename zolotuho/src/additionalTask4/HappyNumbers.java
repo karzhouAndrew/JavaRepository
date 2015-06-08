fb20e49dd1051349538e3ce008f5f6d21e1d5ce2
Zolotuho Sergey <zoltanuga@gmail.com>
2015-06-08 22:24:54 +0300

    Additional task4 Version4

--- zolotuho/src/additionalTask4/HappyNumbers.java
+++ zolotuho/src/additionalTask4/HappyNumbers.java
@@ -9,47 +9,17 @@ public class HappyNumbers {
         System.out.print("Enter three-digit number : ");
         int number = sc.nextInt();
         sc.close();
-        int[] magicArray = new int[]{781, 302, 409, 941};
-        boolean magicNumberPresence = false;
-        for (int i : magicArray) {
-            if (i == number) {
-                magicNumberPresence = true;
-            }
-        }
-        if (magicNumberPresence == true) {
+        boolean magicNumberPresence = number == 781 || number == 302 || number == 409 || number == 941;
+        boolean happyNumberPresence = number == 111 || number == 222 || number == 333 || number == 444 ||
+                number == 555 || number == 666 || number == 777 || number == 888 || number == 999 ||
+                number == 123 || number == 234 || number == 345 || number == 456 || number == 567 ||
+                number == 678 || number == 789;
+        if (happyNumberPresence == true) {
+            System.out.println("Happy");
+        } else if (magicNumberPresence == true) {
             System.out.println("Magic");
         } else {
-            int[] digits = new int[3];
-            int counter = 0;
-            int auxiliaryNum = number;
-            while (auxiliaryNum % 10 != 0) {
-                auxiliaryNum /= 10;
-                counter++;
-            }
-            if (counter == 3) {
-                for (int i = 0; i < counter; i++) {
-                    digits[i] = number % 10;
-                    number -= digits[i];
-                    number /= 10;
-                }
-                int flagHappyEqual = 0;
-                int flagHappyGrow = 0;
-                for (int i = 0; i < counter - 1; i++) {
-                    if (digits[i] == digits[i + 1]) {
-                        flagHappyEqual++;
-                    }
-                    if (digits[i] - digits[i + 1] == 1) {
-                        flagHappyGrow++;
-                    }
-                }
-                if ((flagHappyEqual == 2) || (flagHappyGrow == 2)) {
-                    System.out.println("Happy");
-                } else {
-                    System.out.println("Not happy");
-                }
-            } else {
-                System.out.println("Number is too big or small");
-            }
+            System.out.println("Not happy");
         }
     }
 }
