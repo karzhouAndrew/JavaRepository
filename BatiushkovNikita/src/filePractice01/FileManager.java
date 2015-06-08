7f89d5b024308a06b387e6639600b07a99ba512f
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-06-08 23:36:02 +0300

    FilePractice01

--- BatiushkovNikita/src/filePractice01/FileManager.java
+++ BatiushkovNikita/src/filePractice01/FileManager.java
@@ -1,25 +1,34 @@
 package filePractice01;
 
-// Создать и заполнить файл случайными целыми числами. Отсортировать содержимое файла по возрастанию.
-
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class FileManager {
-    private static final String FILE_PATH = "./BatiushkovNikita/src/filePractice01/Dir/IntegerFile.txt";
-    private final static StringBuilder text = new StringBuilder();
 
-    public static void main(String[] args) {
-        IntegerFile integerFile = new IntegerFile();
-        readFile();
-        List<Integer> list = parseText(text.toString());
-        writeFile(FILE_PATH, sortIntegers(list));
+    private String path;
+
+    public FileManager(String path) {
+        setPath(path);
+        createFile();
     }
 
-    private static void writeFile(String filePath, List<Integer> values) {
-        try (DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(filePath)))) {
-            for (int i = 0; i < values.size(); i++) {
-                dataOutputStream.writeBytes(String.valueOf(values.get(i)));
+    public String[] readFile() {
+        StringBuilder data = new StringBuilder();
+        try (Scanner scanner = new Scanner(new FileReader(path))) {
+            while (scanner.hasNext()) {
+                data.append(scanner.next());
+                data.append(" ");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return data.toString().split("\\s+");
+    }
+
+    public void writeFile(String[] values) {
+        try (DataOutputStream dataOutputStream = (new DataOutputStream(new BufferedOutputStream(new FileOutputStream(path))))) {
+            for (String value : values) {
+                dataOutputStream.writeBytes(value);
                 dataOutputStream.writeBytes(" ");
             }
         } catch (IOException e) {
@@ -28,36 +37,32 @@ public class FileManager {
         }
     }
 
-    private static List<Integer> sortIntegers(List<Integer> values) {
-        Collections.sort(values, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        return values;
-    }
-
-    private static void readFile() {
-        try (Scanner scanner = new Scanner(new FileReader(FILE_PATH))) {
-            while (scanner.hasNext()) {
-                createText(scanner.next());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+    public void removeFile() {
+        try {
+            File newFile = new File(path);
+            newFile.delete();
+        } catch (Exception e) {
+            System.out.println("Cannot remove file.");
+            e.printStackTrace();
         }
     }
 
-    public static void createText(String str) {
-        text.append(str);
-        text.append(" ");
+    public void createFile() {
+        File newFile = new File(path);
+        try {
+            newFile.getParentFile().mkdir();
+            newFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Cannot create file.");
+            e.printStackTrace();
+        }
     }
 
-    public static List<Integer> parseText(String str) {
-        List<Integer> parseValues = new ArrayList<>();
-        for (String value : str.split("\\s+")) {
-            parseValues.add(Integer.valueOf(value));
-        }
-        return parseValues;
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
     }
 }
