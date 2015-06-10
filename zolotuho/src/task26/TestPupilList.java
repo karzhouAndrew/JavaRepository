135bbc9a98bc4c7029ff4987120e45a4746f5cde
Zolotuho Sergey <zoltanuga@gmail.com>
2015-06-10 17:16:57 +0300

    26th task Version3

--- zolotuho/src/task26/TestPupilList.java
+++ zolotuho/src/task26/TestPupilList.java
@@ -1,6 +1,7 @@
 package task26;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -15,11 +16,15 @@ public class TestPupilList {
         pupilList.add(3, new Pupil("Ivan", 6, random.nextFloat() * 10));
         pupilList.add(4, new Pupil("Pofistal", 15, random.nextFloat() * 10));
         pupilList.add(5, new Pupil("Vladlen", 5, random.nextFloat() * 10));
+        Iterator<Pupil> iterator = pupilList.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getAverageMark() <= 3) {
+                iterator.remove();
+            }
+        }
 
         for (Pupil pupil : pupilList) {
-            if (pupil.getAverageMark() <= 3) {
-                System.out.println(pupil);
-            }
+            System.out.println(pupil);
         }
     }
 }
