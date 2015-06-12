05414d92edff89d97b06d4487138269195697b7f
Skrebunov <skrebunov86@mail.ru>
2015-06-13 03:00:31 +0400

    doplab16

--- Skrebunov/src/lab16/Punctuation.java
+++ Skrebunov/src/lab16/Punctuation.java
@@ -4,9 +4,8 @@ package lab16;
 public class Punctuation {
     public int punctuationNumber(String text) {
         int number = 0;
-        char symbol;
         for (int i = 0; i < text.length(); i++) {
-            symbol = text.charAt(i);
+            char symbol = text.charAt(i);
             if (symbol == ',' || symbol == ';' || symbol == '.' || symbol == '!' || symbol == '?' || symbol == ':') {
                 number++;
             }
