b49152a49632f9275be7ffe79e31769bb4861bd2
Skrebunov <skrebunov86@mail.ru>
2015-06-13 02:01:08 +0400

    doplab8ver2

--- Skrebunov/src/doplab8ver2/Main.java
+++ Skrebunov/src/doplab8ver2/Main.java
@@ -4,8 +4,8 @@ package doplab8ver2;
  */
 public class Main {
     public static void main(String[] args) {
-        Change object = new Change();
-        String result = object.changeChildren("12 Child, 1783413 Children jsjsdsjdh 77 jjjj 098? Rtvjj 1, - Child");
+        Change empty = new Change();
+        String result = empty.changeChildren("12 Child, 1783413 Children jsjsdsjdh 77 jjjj 098? Rtvjj 1, - Child");
         System.out.println(result);
     }
 }
