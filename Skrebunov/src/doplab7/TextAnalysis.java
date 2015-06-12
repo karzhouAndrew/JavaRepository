80798a2bde93571df97169f02ea8727f9a596e49
Skrebunov <skrebunov86@mail.ru>
2015-06-13 02:18:37 +0400

    doplab7

--- Skrebunov/src/doplab7/TextAnalysis.java
+++ Skrebunov/src/doplab7/TextAnalysis.java
@@ -6,40 +6,40 @@ import java.util.regex.Pattern;
 
 public class TextAnalysis {
 
-    public String numberOfLetters(String text) {
-        Pattern patternOfVowels = Pattern.compile("[ауоыиэяюе]|[АУОЫИЭЯЮЕ]|[aeiouy]|[AEIOUY]");
-        Matcher matcherOfVowels = patternOfVowels.matcher(text);
+    public String lettersNumber(String text) {
+        Pattern vowelsPattern = Pattern.compile("[ауоыиэяюе]|[АУОЫИЭЯЮЕ]|[aeiouy]|[AEIOUY]");
+        Matcher vowelsMatcher = vowelsPattern.matcher(text);
         int vowels = 0;
-        while (matcherOfVowels.find()) {
+        while (vowelsMatcher.find()) {
             vowels++;
         }
-        matcherOfVowels.reset();
-        Pattern patternOfConsonants = Pattern.compile("[бвгджзйклмнпрстфхцчшщ]|[БВГДЖЗЙКЛМНПРСТФХЦЧШЩ]|[bcdfghjklmnpqrstv]|[BCDFGHJKLMNPQRSTV]");
-        Matcher matcherOfConsonants = patternOfConsonants.matcher(text);
+        vowelsMatcher.reset();
+        Pattern consonantsPattern = Pattern.compile("[бвгджзйклмнпрстфхцчшщ]|[БВГДЖЗЙКЛМНПРСТФХЦЧШЩ]|[bcdfghjklmnpqrstv]|[BCDFGHJKLMNPQRSTV]");
+        Matcher consonantsMatcher = consonantsPattern.matcher(text);
         int consonants = 0;
-        while (matcherOfConsonants.find()) {
+        while (consonantsMatcher.find()) {
             consonants++;
         }
         return "Гласных " + vowels + " Согласных " + consonants;
     }
 
-    public void numberOfSymbolsInSentences(String text) {
-        Pattern pattern = Pattern.compile("[a-zA-Z\\s\\w\\d\\-\\+,()[а-яА-Я\\s\\w\\d]]+");
-        Matcher matcher = pattern.matcher(text);
+    public void symbolsNumberInSentences(String text) {
+        Pattern symbolPattern = Pattern.compile("[a-zA-Z\\s\\w\\d\\-\\+,()[а-яА-Я\\s\\w\\d]]+");
+        Matcher symbolMatcher = symbolPattern.matcher(text);
         int lettersInSentences = 0;
-        while (matcher.find()) {
-            System.out.println(matcher.group() + ". Количество символов - " + matcher.group().length());
+        while (symbolMatcher.find()) {
+            System.out.println(symbolMatcher.group() + ". Количество символов - " + symbolMatcher.group().length());
         }
     }
 
     public int spacesNumber(String text) {
-        Pattern pattern = Pattern.compile("\\s|\n");
-        Matcher matcher = pattern.matcher(text);
-        int numberOfSpaces = 0;
-        while (matcher.find()) {
-            numberOfSpaces++;
+        Pattern spacesPattern = Pattern.compile("\\s|\n");
+        Matcher spacesMatcher = spacesPattern.matcher(text);
+        int spacesCount = 0;
+        while (spacesMatcher.find()) {
+            spacesCount++;
         }
-        return numberOfSpaces;
+        return spacesCount;
     }
 
     public String textWithoutSpaces(String text) {
