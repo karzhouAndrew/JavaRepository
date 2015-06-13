646e2a7032989b1fcd25331e6069fd3a8f4fc38a
shadow5551 <yanusikdima@mail.ru>
2015-06-13 21:17:39 +0300

    Update DifferentDigit.java

--- yanusik/src/DifferentDigit.java
+++ yanusik/src/DifferentDigit.java
@@ -7,27 +7,27 @@ public class DifferentDigit {
     public static void main(String[] args) {
         int arrValue[] = new int[25];
         int value = 11;
-        int kolCifr = 0;
-        int flag = 0;
-        for (int i = 10, val = value; val > 0; i = i * 10) {
-            val = val - i;
-            kolCifr++;
+        int valueDigit = 0;
+        boolean flag = false;
+        for (int i = 10, j = value; j > 0; i = i * 10) {
+            j = j - i;
+            valueDigit++;
         }
         int counter = 0;
-        for (int val = value, ost, i = 10; kolCifr != 0; kolCifr--, i = i * 10, counter++) {
+        for (int j = value, ost, i = 10; kolCifr != 0; kolCifr--, i = i * 10, counter++) {
             ost = (val % i) / (i / 10);
-            val = val - ost;
+            j = j - ost;
             arrValue[counter] = ost;
         }
         for (int i = 0; i < counter; i++) {
             for (int k = i + 1; k < counter; k++) {
                 if (arrValue[i] == arrValue[k]) {
 
-                    flag++;
+                    flag = true;
                 }
             }
         }
-        if (flag != 0) {
+        if (flag) {
             System.out.println("не все цифры числа различны");
         } else {
             System.out.println("все цифры числа различны");
