35c7173784e2296bc6b356e1a630d78d5649b578
shadow5551 <yanusikdima@mail.ru>
2015-06-15 13:36:27 +0300

    Update Factorial.java

--- yanusik/src/Factorial.java
+++ yanusik/src/Factorial.java
@@ -1,3 +1,5 @@
+package lab8;
+
 /**
  * Изменить  пример  с  суммой  чисел  таким  образом,  чтобы
  * рассчитывалась не сумма, а произведение, т.е. факто
@@ -10,12 +12,12 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.printf("enter the value ");
-        int value = in.nextInt();
+        int number = in.nextInt();
+        in.close();
         long factorial = 1;
-        for (int i = 1; i <= value; i++) {
+        for (int i = 1; i <= number; i++) {
             factorial *= i;
         }
         System.out.println(factorial);
-        in.close();
     }
 }
