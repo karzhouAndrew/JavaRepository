16c1728925de0dfe7715373cbd00fafc479de861
shadow5551 <yanusikdima@mail.ru>
2015-06-15 03:24:09 +0300

    Update TestString.java

--- yanusik/src/Count/TestString.java
+++ yanusik/src/Count/TestString.java
@@ -9,7 +9,7 @@ public class TestString {
         String str = " Ниже не приводятся общеизвестные и очевидные сочетания вроде Ctr+C, " +
                 "Ctr+V или Ctr + S. В IntelliJ IDEA многие хоткеи имеют парный хоткей отличающийся" +
                 " только тем, что в нем дополнительно участвует клавиша Shift. ";
-        System.out.println("number of punctuation = " + string.CountPun(str));
+        System.out.println("number of punctuation = " + string.countPun(str));
 
     }
 }
