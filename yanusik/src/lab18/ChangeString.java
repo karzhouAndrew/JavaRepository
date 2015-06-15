784f7fec887eacc47bcd5e340ed98ef1c2d513a5
dmitry yanusik <yanusikdima@mail.ru>
2015-06-15 12:53:34 +0300

    lab18

--- yanusik/src/lab18/ChangeString.java
+++ yanusik/src/lab18/ChangeString.java
@@ -6,8 +6,8 @@ package lab18;
 public class ChangeString {
 
     public void newString(String newString) {
-        char symbol;
         for (int i = 0; i < newString.length(); i++) {
+            char symbol;
             symbol = newString.charAt(i);
             if (symbol == ' ' || symbol == '\0') {
                 System.out.print(newString.charAt(i - 1));
