29a87539841c48c7ebd94532ff2e9962b7f4b9b2
Константин <k336699k@mail.ru>
2015-06-16 22:32:01 +0300

    Лабораторная работа 25

---
+++ Shaplyko/src/lab25/CreditCard.java
@@ -0,0 +1,25 @@
+package lab25;
+
+public class CreditCard extends Card {
+    private String personalData;
+
+    public CreditCard(int identificationNumber, String personalData) {
+        super(identificationNumber);
+        this.personalData = personalData;
+    }
+
+    public String getPersonalData() {
+        return personalData;
+    }
+
+    public void setPersonalData(String personalData) {
+        this.personalData = personalData;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "personalData='" + personalData + '\'' +
+                '}';
+    }
+}
