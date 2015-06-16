008d191b4f077434b097dfab2c338e4961c4b2f7
Константин <k336699k@mail.ru>
2015-06-16 21:44:44 +0300

    Дополнительное задание на строки 6

---
+++ Shaplyko/src/str6/Find.java
@@ -0,0 +1,20 @@
+package str6;
+
+public class Find {
+    public static final String DIVIDED_PUNCTUATION = "[-;:,.!?\\s]";
+
+    public String findMirror(String str) {
+        String strWord[] = str.split(DIVIDED_PUNCTUATION);
+        int lengthMax = 0;
+        String result = null;
+        for (int i = 0; i < strWord.length; i++) {
+            if (strWord[i].equals((new StringBuilder(strWord[i])).reverse().toString()) && strWord[i].length() > lengthMax) {
+                result = strWord[i];
+                lengthMax = strWord[i].length();
+            }
+        }
+        return result;
+    }
+}
+
+
