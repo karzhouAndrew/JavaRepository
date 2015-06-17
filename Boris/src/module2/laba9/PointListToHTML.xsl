46c8b99e0df026d26cd3b979c2e899353f67e00d
Bronic86 <ezhik_b_tymane@mail.ru>
2015-06-17 23:08:44 +0300

    Module 2 task 9.

---
+++ Boris/src/module2/laba9/PointListToHTML.xsl
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:output method="html"/>
+    <xsl:template match="/">
+        <head>
+            <body>
+                <table border="1" bgcolor="BLACK">
+                    <caption>
+                        <font color="RED">PointsList</font>
+                    </caption>
+                    <tr align="centre" bgcolor="BLUE">
+                        <th>
+                            <font color="YELLOW">X</font>
+                        </th>
+                        <th>
+                            <font color="YELLOW">Y</font>
+                        </th>
+                    </tr>
+                    <xsl:apply-templates/>
+                </table>
+            </body>
+        </head>
+    </xsl:template>
+
+    <xsl:template match="point">
+        <tr bgcolor="WHITE">
+            <xsl:apply-templates/>
+        </tr>
+    </xsl:template>
+
+    <xsl:template match="x">
+        <td align="centre">
+            <font color="GREEN">
+                <xsl:value-of select="text()"/>
+                <span>&#160;</span>
+                <xsl:value-of select="../attribute::units"/>
+            </font>
+        </td>
+    </xsl:template>
+
+    <xsl:template match="y">
+        <td align="centre">
+            <font color="GREEN">
+                <xsl:value-of select="text()"/>
+                <span>&#160;</span>
+                <xsl:value-of select="../attribute::units"/>
+            </font>
+        </td>
+    </xsl:template>
+
+</xsl:stylesheet>
---
+++ Boris/src/module2/laba9/TransformXMLtoXSLT.java
@@ -0,0 +1,43 @@
+package module2.laba9;
+
+//Напишите программу, преобразующую созданный в предыдущих за-даниях xml файл в html, аналогично
+// данному примеру, в каждой ячейке дополнительно к координате должны выводиться единицы измерения.
+// Результат должен записываться в файл на диске.
+
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class TransformXMLtoXSLT {
+    public void transform(String xmlPath, String xslPath) throws TransformerException, FileNotFoundException {
+        File xmlFile = new File(xmlPath);
+        if (xmlFile.getName().endsWith(".xml$")) {
+            throw new IllegalArgumentException(" Not \".xml\" file.");
+        }
+        File xslFile = new File(xslPath);
+        if (xslFile.getName().endsWith(".xsl$")) {
+            throw new IllegalArgumentException(" Not \".xsl\" file.");
+        }
+        if (xmlFile.exists()) {
+            if (xslFile.exists()) {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                StreamSource streamSource = new StreamSource(xslFile);
+                Templates templates = transformerFactory.newTemplates(streamSource);
+                Transformer transformer = templates.newTransformer();
+                transformer.transform(new StreamSource(xmlFile), new StreamResult(new FileOutputStream(new File(
+                        xmlFile.getPath().substring(0, xmlFile.getPath().length() - 3) + "html"))));
+            } else {
+                throw new FileNotFoundException(xslPath);
+            }
+        } else {
+            throw new FileNotFoundException(xmlPath);
+        }
+    }
+
+}
