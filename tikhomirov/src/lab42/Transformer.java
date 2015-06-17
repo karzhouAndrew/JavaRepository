cc631ad6c30c28f1f83fdc4ec339382b1157a518
MaxTsikhamirau <m.tsikgamirau@gmail.com>
2015-06-18 00:16:08 +0300

    Task #42 XSL

---
+++ tikhomirov/src/lab42/Transformer.java
@@ -0,0 +1,24 @@
+package lab42;
+
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Transformer {
+
+    public void transform(String XSL_File, String XML_File) throws TransformerException, FileNotFoundException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        StreamSource streamSource = new StreamSource(new File(XSL_File));
+        Templates templates = transformerFactory.newTemplates(streamSource);
+        javax.xml.transform.Transformer transformer = templates.newTransformer();
+        transformer.transform(new StreamSource(new File(XML_File)),
+                new StreamResult(new FileOutputStream
+                        (new File("D:\\env\\Java\\JavaRepository_1\\tikhomirov\\src\\lab42\\Example.html"))));
+    }
+}
---
+++ tikhomirov/src/lab42/TransformerTest.java
@@ -0,0 +1,13 @@
+package lab42;
+
+import javax.xml.transform.TransformerException;
+import java.io.FileNotFoundException;
+
+public class TransformerTest {
+    public static void main(String[] args) throws TransformerException, FileNotFoundException {
+        String XSL_File = "D:\\env\\Java\\JavaRepository_1\\tikhomirov\\src\\lab42\\example.xsl";
+        String XML_File = "D:\\env\\Java\\JavaRepository_1\\tikhomirov\\src\\lab42\\example.xml";
+        Transformer transformer = new Transformer();
+        transformer.transform(XSL_File, XML_File);
+    }
+}
