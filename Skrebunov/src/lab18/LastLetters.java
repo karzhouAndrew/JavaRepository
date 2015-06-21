4d3a502ee761cf3c516f7d21b7ddf5dbfbf8ef62
Skrebunov <skrebunov86@mail.ru>
2015-06-22 00:59:56 +0400

    lab18

--- Skrebunov/src/lab18/LastLetters.java
+++ Skrebunov/src/lab18/LastLetters.java
@@ -3,9 +3,8 @@ package lab18;
 
 public class LastLetters {
     public void lastLetterToWord(String text) {
-        char symbol;
         for (int i = 0; i < text.length(); i++) {
-            symbol = text.charAt(i);
+            char symbol = text.charAt(i);
             if (symbol != ' ' && symbol != '?' && symbol != '!' && symbol != ',' && symbol != '.') {
                 if (text.charAt(i + 1) == ' ' || text.charAt(i + 1) == '?' || text.charAt(i + 1) == '!' || text.charAt(i + 1) == ',' || text.charAt(i + 1) == '.') {
                     System.out.print(symbol);
