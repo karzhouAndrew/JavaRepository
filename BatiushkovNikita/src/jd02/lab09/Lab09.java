d9aa68dbd60bbf6d7575455cde38a8b143bba286
BatiushkovNikita <batiushkov.nik@gmail.com>
2015-06-22 11:13:13 +0300

    Lab09 JD02

---
+++ BatiushkovNikita/src/jd02/lab09/Lab09.java
@@ -0,0 +1,22 @@
+package jd02.lab09;
+
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Lab09 {
+    public static void main(String[] args) throws TransformerException, FileNotFoundException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        StreamSource streamSource = new StreamSource(new File("./BatiushkovNikita/src/jd02/lab09/Lab09.xsl"));
+        Templates templates = transformerFactory.newTemplates(streamSource);
+        Transformer transformer = templates.newTransformer();
+        transformer.transform(new StreamSource(new File("./BatiushkovNikita/src/jd02/lab09/Lab09.xml")),
+                new StreamResult(new FileOutputStream(new File("./BatiushkovNikita/src/jd02/lab09/Lab09.html"))));
+    }
+}
