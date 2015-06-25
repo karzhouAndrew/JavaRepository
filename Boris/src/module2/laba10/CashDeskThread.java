e5f8278be5715ee7d0586968d3e55791aa2caf58
Bronic86 <ezhik_b_tymane@mail.ru>
2015-06-26 00:21:32 +0300

    Module 2 Task 10

--- Boris/src/module2/laba10/CashDeskThread.java
+++ Boris/src/module2/laba10/CashDeskThread.java
@@ -1,17 +1,21 @@
 package module2.laba10;
 
 
-import module2.laba10.customer.SeeCustomer;
-
+import module2.laba10.customer.Customer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CashDeskThread {
+    private static final String THREAD_NUMBER_REGEX = "thread-(\\d+)";
     private static CashDeskThread instance;
-    private static final int POOLS = 10;
+    private static final int THREADS = 10;
+    ExecutorService poolThreads;
 
     private CashDeskThread() {
-
+        poolThreads = Executors.newFixedThreadPool(THREADS);
     }
 
     public static CashDeskThread getInstance() {
@@ -22,22 +26,28 @@ public class CashDeskThread {
         return instance;
     }
 
-    public void work(final SeeCustomer customer) {
-        ExecutorService poolThreads;
+    public void work(final Customer customer) {
         synchronized (customer) {
-            poolThreads = Executors.newFixedThreadPool(POOLS);
             poolThreads.execute(new Runnable() {
                 @Override
                 public void run() {
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(customer.getCustomer() + "\n");
+                    Matcher matcher = Pattern.compile(THREAD_NUMBER_REGEX).matcher(Thread.currentThread().getName());
+                    matcher.find();
+                    System.out.println("CashDesk #" + matcher.group(1) + customer + "\n");
                 }
             });
         }
-        poolThreads.shutdown();
     }
+
+    public void shutdown(){
+//        try {
+            while (!poolThreads.isTerminated()){
+                poolThreads.shutdown();
+            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("Goodbye.");
+    }
+
 }
--- Boris/src/module2/laba10/TestThreads.java
+++ Boris/src/module2/laba10/TestThreads.java
@@ -5,10 +5,10 @@ import module2.laba10.customer.Customer;
 
 public class TestThreads {
     public static void main(String[] args) {
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 500; i++) {
             Customer customer = new Customer();
-            customer.generateProducts((int) (Math.random() * 5));
             CashDeskThread.getInstance().work(customer);
         }
+        CashDeskThread.getInstance().shutdown();
     }
 }
