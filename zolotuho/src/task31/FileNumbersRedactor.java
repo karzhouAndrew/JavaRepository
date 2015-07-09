5fbac51bab5890f1f0c1c102560e7d11aa5e191e
Zolotuho Sergey <zoltanuga@gmail.com>
2015-07-09 23:09:05 +0300

    31t task Version2

--- zolotuho/src/task31/FileNumbersRedactor.java
+++ zolotuho/src/task31/FileNumbersRedactor.java
@@ -4,24 +4,27 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //Имеется файл с текстом, в котором присутствуют числа.
 // Найти все числа, распечатать, посчитать сумму, убрать все повторяющиеся числа и снова распечатать.
 public class FileNumbersRedactor {
-    private final String numbersRegex = "^-?\\d+$|^-?\\d+[.]\\d+$";
-    private final String wordsRegex = "[.,:;][ ]+|[ ]+|[.!?]$";
+    private final String NUMBERS_REGEX = "^-?\\d+$|^-?\\d+[.]\\d+$";
+    private final String WORDS_REGEX = "[.,:;][ ]+|[ ]+|[.!?]$";
 
     public List<Double> findNumbers(File path) throws IOException, NumberFormatException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         String currentString = bufferedReader.readLine();
-        Pattern pattern = Pattern.compile(numbersRegex);
+        Pattern pattern = Pattern.compile(NUMBERS_REGEX);
         List<Double> numbersFromText = new ArrayList<Double>();
         while (currentString != null) {
             currentString = currentString.trim();
-            String[] words = currentString.split(wordsRegex);
+            String[] words = currentString.split(WORDS_REGEX);
             for (int i = 0; i < words.length; i++) {
                 Matcher matcher = pattern.matcher(words[i]);
                 if (matcher.find()) {
@@ -35,8 +38,8 @@ public class FileNumbersRedactor {
         return numbersFromText;
     }
 
-    public Set<Double> convertToSet(List<Double> numbersFromText) {
-        Set<Double> uniqueNumbersFromText = new HashSet<Double>(numbersFromText);
+    public HashSet<Double> convertToSet(List<Double> numbersFromText) {
+        HashSet<Double> uniqueNumbersFromText = new HashSet<Double>(numbersFromText);
         return uniqueNumbersFromText;
     }
 
