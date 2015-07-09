9b886a8c6bfb0bc584f3bc075242036152e5ee61
Zolotuho Sergey <zoltanuga@gmail.com>
2015-07-09 22:51:21 +0300

    23d task Version4

--- zolotuho/src/task23/TestCashMachine.java
+++ zolotuho/src/task23/TestCashMachine.java
@@ -5,7 +5,7 @@ public class TestCashMachine {
     public static void main(String[] args) {
         CashMachine cashMachine = new CashMachine(10, 10, 10);
         cashMachine.withdrawCash(1170);
-        System.out.println(cashMachine.toString());
+        System.out.println(cashMachine);
         cashMachine.addDenominations(100, 300, 500);
         System.out.println(cashMachine);
         cashMachine.withdrawCash(5050);
