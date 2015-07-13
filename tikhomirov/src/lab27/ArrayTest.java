c11e3913b0869a02b129042d91221e3d4d477ee0
MaxTsikhamirau <m.tsikgamirau@gmail.com>
2015-07-13 18:10:22 +0300

    Task #27 Edit#2

--- tikhomirov/src/lab27/ArrayTest.java
+++ tikhomirov/src/lab27/ArrayTest.java
@@ -8,30 +8,30 @@ import java.util.List;
  */
 
 public class ArrayTest {
-    static final int MAX_VALUE = 8;
-    static final int NUMBER = 10;
+    private static final int MAX_VALUE = 8;
+    private static final int NUMBER = 10;
 
     public static void main(String[] args) {
-        List<Integer> arrayList = new ArrayList<Integer>();
+        List<Integer> numbersList = new ArrayList<Integer>();
         for (int i = 0; i < NUMBER; i++) {
-            arrayList.add((int) (Math.random() * MAX_VALUE));
+            numbersList.add((int) (Math.random() * MAX_VALUE));
         }
         System.out.println("Первоначальный массив");
-        System.out.println("arrayList = " + arrayList);
+        System.out.println("numbersList = " + numbersList);
         int counter = 0;
-        for (int i = 0; i < arrayList.size(); i++) {
-            for (int j = i + 1; j < arrayList.size(); j++) {
-                if (arrayList.get(i).equals(arrayList.get(j))) {
+        for (int i = 0; i < numbersList.size(); i++) {
+            for (int j = i + 1; j < numbersList.size(); j++) {
+                if (numbersList.get(i).equals(numbersList.get(j))) {
                     counter++;
                 }
                 if (counter == 1) {
-                    arrayList.remove(j);
+                    numbersList.remove(j);
                     counter = 0;
                     j--;
                 }
             }
         }
         System.out.println("Без повторяющихся элементов");
-        System.out.println("arrayList = " + arrayList);
+        System.out.println("numbersList = " + numbersList);
     }
 }
