bc8b548ce11970a1affdec7fc404f6ca99db0c5b
Vitaly Likhanov <vitaly2113@gmail.com>
2015-07-29 18:12:28 +0300

    исправил

--- Likhanov/src/lab3/find7.java
+++ Likhanov/src/lab3/find7.java
@@ -1,10 +1,10 @@
 package lab3;
 
-//ќпределите, €вл€етс€ ли последн€€ цифра числа семеркой
+//find out if the last digit is seven
 public class find7 {
     public static void main(String[] args) {
-        int a = 217;
-        if (a % 10 == 7) {
+        int num = 217;
+        if (num % 10 == 7) {
             System.out.println("The last digit is 7");
         } else {
             System.out.println("The last digit is not 7");
