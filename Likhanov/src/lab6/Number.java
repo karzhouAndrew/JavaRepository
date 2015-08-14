b1a8793cbd5596c0650e6f8dbdc21032810f28ae
Vitaly Likhanov <vitaly2113@gmail.com>
2015-08-14 13:04:11 +0300

    Задание 11 Найдите сумму первых n целых чисел, которые делятся на 3.

--- Likhanov/src/lab6/Number.java
+++ Likhanov/src/lab6/Number.java
@@ -1,16 +1,19 @@
 package lab6;
 
-// Определить число, полученное выписыванием в обратном порядке цифр любого натурального числа n.
+import java.util.Scanner;
+
+// Determine the number obtained by writing the digits in a reverse order of any natural number n
 
 public class Number {
     public static void main(String[] args) {
-        int number = 123;
-        int finalnum;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int finalNum;
         while (number > 0) {
-            finalnum = number % 10;
+            finalNum = number % 10;
             number = number / 10;
-            if (finalnum > 0) {
-                System.out.print(finalnum);
+            if (finalNum > 0) {
+                System.out.print(finalNum);
             } else {
                 System.out.print("");
             }
