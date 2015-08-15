559edd24364dcf4a90d104384f150a52cdd1c457
dudinsky <dudinsky@tut.by>
2015-08-15 20:32:41 +0300

    Task30_version_3

--- Dudinsky/src/lab30/Task30.java
+++ Dudinsky/src/lab30/Task30.java
@@ -3,10 +3,11 @@ package lab30;
 /* Задание 30. Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов. */
 
 public class Task30 {
+
+    private final static String INPUT_TXT_FILE_PATH = "./Dudinsky/src/lab30/textFileToTest.txt";
+
     public static void main(String[] args) {
-
-        final String INPUT_TXT_FILE_PATH = "./Dudinsky/src/lab30/textFileToTest.txt";
-
+        
         TextProcessor textProcessor = new TextProcessor();
         textProcessor.readText(INPUT_TXT_FILE_PATH);
         System.out.print("В файле \'" + INPUT_TXT_FILE_PATH + "\' обнаружено ");
