1d5509777e6d80f0117b31733753ff276b94a326
dudinsky <dudinsky@tut.by>
2015-10-12 18:37:52 +0300

    JD02_Task01_v2

--- Dudinsky/src/jd02lab01/JD02Task01.java
+++ Dudinsky/src/jd02lab01/JD02Task01.java
@@ -6,12 +6,12 @@ import java.util.Locale;
 
 public class JD02Task01 {
 
-    private static final double amount = 153.5;
+    private static final double AMOUNT = 153.5;
     private static final String LANGUAGE = "uk";
     private static final String REGION = "UA";
 
     public static void main(String[] args) {
         Locale locale = new Locale(LANGUAGE, REGION);
-        System.out.println(CurrencyFormatter.localize(locale, amount));
+        System.out.println(CurrencyFormatter.localize(locale, AMOUNT));
     }
 }
