a9161bcab7100722a16a57a364e286b1c37aed84
dudinsky <dudinsky@tut.by>
2015-10-12 19:08:59 +0300

    JD03_Task10_v2

--- Dudinsky/src/jd03lab10/src/jd03lab10/FormProcessor.java
+++ Dudinsky/src/jd03lab10/src/jd03lab10/FormProcessor.java
@@ -24,15 +24,15 @@ public class FormProcessor extends HttpServlet {
         request.setCharacterEncoding("utf-8");
         String userName = request.getParameter("username");
         String phoneNumber = request.getParameter("phoneN");
-        String eMail = request.getParameter("e-mail");
+        String email = request.getParameter("e-mail");
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html><html><head lang=\"ru\"><meta charset=\"UTF-8\">");
-        if (userName.isEmpty() || (phoneNumber.isEmpty() && eMail.isEmpty())) {
+        if (userName.isEmpty() || (phoneNumber.isEmpty() && email.isEmpty())) {
             printErrorMessageWithHRef(out, request);
         } else {
-            printValidDataAnswer(out, userName, phoneNumber, eMail);
+            printValidDataAnswer(out, userName, phoneNumber, email);
         }
         out.println("</body></html>");
         out.close();
@@ -47,11 +47,11 @@ public class FormProcessor extends HttpServlet {
         out.print("<p>Либо вернитесь браузером на предыдущую страницу (в этом случае в полях будут ранее введенные данные)</p>");
     }
 
-    private void printValidDataAnswer(PrintWriter out, String userName, String phoneNumber, String eMail) {
+    private void printValidDataAnswer(PrintWriter out, String userName, String phoneNumber, String email) {
         out.println("<title>Заполнение формы успешно завершено</title></head>");
         out.print("<body><h1>Введенные данные успешно приняты:</h1>");
         out.print("<p>Имя пользователя: " + userName + "</p>");
         out.print("<p>Номер телефона: " + (phoneNumber.isEmpty() ? "не указан" : phoneNumber) + "</p>");
-        out.print("<p>Адрес электронной почты: " + (eMail.isEmpty() ? "не указан" : eMail) + "</p>");
+        out.print("<p>Адрес электронной почты: " + (email.isEmpty() ? "не указан" : email) + "</p>");
     }
 }
