ed70fa8171bb767e97c8a3e8ff4b5f2058e69d95
Vitaly Likhanov <vitaly2113@gmail.com>
2015-11-02 17:01:20 +0300

    There is  time interval in seconds. It should be changed  in the form of weeks, days, hours, minutes and seconds.

--- Likhanov/src/lab2/HellowWorld.java
+++ Likhanov/src/lab2/HellowWorld.java
@@ -1,16 +1,21 @@
 package lab2;
+
+import java.util.Scanner;
+
 //task 2
 public class HellowWorld {
     public static void main(String[] args) {
-        int s = 5566600;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int s = scanner.nextInt();
         int sec = s % 60;
         int m = (s - sec) / 60;
         int min = m % 60;
         int h = (m - min) / 60;
-        int d = h % 24;
-        int day = (h - d) / 24;
-        int w = day % 7;
-        int week = (day - w) / 7;
-        System.out.println(week + " weeks " + day + " days " + h + " hours " + min + " minutes " + sec + " seconds");
+        int hour = h % 24;
+        int d = (h - hour) / 24;
+        int day = d % 7;
+        int week = (d - day) / 7;
+        System.out.println(week + " weeks " + day + " days " + hour + " hours " + min + " minutes " + sec + " seconds");
     }
 }
