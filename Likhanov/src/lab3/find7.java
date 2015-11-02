b62bad00f7db2a2277dd95f25845f84f661902c0
Vitaly Likhanov <vitaly2113@gmail.com>
2015-11-02 17:08:42 +0300

    Find out if the last digit is seven

--- Likhanov/src/lab3/find7.java
+++ Likhanov/src/lab3/find7.java
@@ -1,9 +1,13 @@
 package lab3;
 
+import java.util.Scanner;
+
 //find out if the last digit is seven
 public class find7 {
     public static void main(String[] args) {
-        int num = 217;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print the number: ");
+        int num = scanner.nextInt();
         if (num % 10 == 7) {
             System.out.println("The last digit is 7");
         } else {
