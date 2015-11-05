09150030d87d27f2a929fd94688d27fc19e986b5
Vitaly Likhanov <vitaly2113@gmail.com>
2015-11-05 12:02:46 +0300

    Create two dimensional square array and fill it in a butterfly style.

--- Likhanov/src/lab15/Butterfly.java
+++ Likhanov/src/lab15/Butterfly.java
@@ -13,35 +13,16 @@ public class Butterfly {
         int columns = scanner.nextInt();
         int[][] butterfly = new int[lines][columns];
         System.out.println("And here is a butterfly!");
-        for (int i = 0; i < butterfly.length / 2; i++) {
-            for (int k = 0; k < butterfly.length; k++) {
-                if (k < i || k >= butterfly.length - i) {
-                    butterfly[i][k] = 0;
-                } else {
-                    butterfly[i][k] = 1;
-                }
-
+        for (int i = 0; i <= butterfly.length / 2; i++) {
+            for (int k = i; k < butterfly.length - i; k++) {
+                butterfly[i][k] = 1;
+                butterfly[butterfly.length - 1 - i][k] = 1;
             }
-
         }
-        for (int i = butterfly.length - 1; i >= butterfly.length / 2; i--) {
-            for (int k = 0; k < butterfly.length; k++) {
-                if (k > i || k < butterfly.length - (i + 1)) {
-                    butterfly[i][k] = 0;
-                } else {
-                    butterfly[i][k] = 1;
-                }
-
-            }
-
-        }
-        for (int i = 0; i < butterfly.length; i++) {
-            for (int j = 0; j < butterfly.length; j++) {
-                System.out.print(butterfly[i][j]);
-            }
-            System.out.println();
-        }
+        System.out.println(Arrays.deepToString(butterfly));
+        scanner.close();
     }
 }
+
 
 
